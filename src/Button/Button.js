3b2317175bf659fd856728fbf2fcd8e87572cd9a
3LOK <yoava@wix.com>
2016-11-22 09:52:48 +0200

    button height values changed to small/medium and large

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -11,7 +11,8 @@ export default function Button(props) {
         [styles[style]]:true,
         [styles.hover]: hover,
         [styles.active]:active,
-        [styles.disabled]:disabled
+        [styles.disabled]:disabled,
+        [styles[`height${height}`]]: true
     });
 
     const _style = {
@@ -30,11 +31,11 @@ export default function Button(props) {
 Button.displayName = 'Button';
 Button.defaultProps = {
     style: 'fullblue',
-    height: 36
+    height: 'medium'
 }
 Button.propTypes = {
     style: React.PropTypes.oneOf(['fullblue', 'emptyblue', 'fullpurple', 'emptypurple', 'fullgreen', 'emptygreen', 'fullred', 'emptyred']).isRequired,
-    height: React.PropTypes.number,
+    height: React.PropTypes.oneOf(['small', 'medium', 'large']),
     hover: React.PropTypes.bool,
     active: React.PropTypes.bool,
     disabled: React.PropTypes.bool,
--- stories/Button.js
+++ stories/Button.js
@@ -27,8 +27,8 @@ export default () => {
                     </tr>
                     <tr>
                         <td>height</td>
-                        <td>number - 30, 36, 42 are the standard</td>
-                        <td>Default is 36</td>
+                        <td>string - small (30), medium (36), large (42)</td>
+                        <td>Default is medium</td>
                     </tr>
                     <tr>
                         <td>disabled</td>
@@ -46,57 +46,75 @@ export default () => {
             <h3>Usage Examples</h3>
 
             <h4>Style = fullblue</h4>
-            Standard: <Button style={'fullblue'} >Click Me!</Button>
-            Hover: <Button style={'fullblue'} hover={true} >Click Me!</Button>
-            Active: <Button style={'fullblue'} active={true} >Click Me!</Button>
-            Disabled : <Button style={'fullblue'} disabled={true} >Click Me!</Button>
+            <div style={{display:'flex', alignItems:'center', justifyContent:'space-between'}}>
+            <div>Standard: <Button style={'fullblue'} >Click Me!</Button></div>
+            <div>Hover: <Button style={'fullblue'} hover={true} >Click Me!</Button></div>
+            <div>Active: <Button style={'fullblue'} active={true} >Click Me!</Button></div>
+            <div>Disabled : <Button style={'fullblue'} disabled={true} >Click Me!</Button></div>
+            </div>
 
             <h4>Style = emptyblue</h4>
-            Standard: <Button style={'emptyblue'} >Click Me!</Button>
-            Hover: <Button style={'emptyblue'} hover={true} >Click Me!</Button>
-            Active: <Button style={'emptyblue'} active={true} >Click Me!</Button>
-            Disabled: <Button style={'emptyblue'} disabled={true} >Click Me!</Button>
+            <div style={{display:'flex', alignItems:'center', justifyContent:'space-between'}}>
+            <div>Standard: <Button style={'emptyblue'} >Click Me!</Button></div>
+            <div>Hover: <Button style={'emptyblue'} hover={true} >Click Me!</Button></div>
+            <div>Active: <Button style={'emptyblue'} active={true} >Click Me!</Button></div>
+            <div>Disabled: <Button style={'emptyblue'} disabled={true} >Click Me!</Button></div>
+            </div>
 
             <h4>Style = fullpurple</h4>
-            Standard: <Button style={'fullpurple'} >Click Me!</Button>
-            Hover: <Button style={'fullpurple'} hover={true} >Click Me!</Button>
-            Active: <Button style={'fullpurple'} active={true} >Click Me!</Button>
-            Disabled: <Button style={'fullpurple'} disabled={true} >Click Me!</Button>
+            <div style={{display:'flex', alignItems:'center', justifyContent:'space-between'}}>
+            <div>Standard: <Button style={'fullpurple'} >Click Me!</Button></div>
+            <div>Hover: <Button style={'fullpurple'} hover={true} >Click Me!</Button></div>
+            <div>Active: <Button style={'fullpurple'} active={true} >Click Me!</Button></div>
+            <div>Disabled: <Button style={'fullpurple'} disabled={true} >Click Me!</Button></div>
+            </div>
 
             <h4>Style = emptypurple</h4>
-            Standard: <Button style={'emptypurple'} >Click Me!</Button>
-            Hover: <Button style={'emptypurple'} hover={true} >Click Me!</Button>
-            Active: <Button style={'emptypurple'} active={true} >Click Me!</Button>
-            Disabled: <Button style={'emptypurple'} disabled={true} >Click Me!</Button>
+            <div style={{display:'flex', alignItems:'center', justifyContent:'space-between'}}>
+            <div>Standard: <Button style={'emptypurple'} >Click Me!</Button></div>
+            <div>Hover: <Button style={'emptypurple'} hover={true} >Click Me!</Button></div>
+            <div>Active: <Button style={'emptypurple'} active={true} >Click Me!</Button></div>
+            <div>Disabled: <Button style={'emptypurple'} disabled={true} >Click Me!</Button></div>
+            </div>
 
             <h4>Style = fullgreen</h4>
-            Standard: <Button style={'fullgreen'} >Click Me!</Button>
-            Hover: <Button style={'fullgreen'} hover={true} >Click Me!</Button>
-            Active: <Button style={'fullgreen'} active={true} >Click Me!</Button>
-            Disabled: <Button style={'fullgreen'} disabled={true} >Click Me!</Button>
+            <div style={{display:'flex', alignItems:'center', justifyContent:'space-between'}}>
+            <div>Standard: <Button style={'fullgreen'} >Click Me!</Button></div>
+            <div>Hover: <Button style={'fullgreen'} hover={true} >Click Me!</Button></div>
+            <div>Active: <Button style={'fullgreen'} active={true} >Click Me!</Button></div>
+            <div>Disabled: <Button style={'fullgreen'} disabled={true} >Click Me!</Button></div>
+            </div>
 
             <h4>Style = emptygreen</h4>
-            Standard: <Button style={'emptygreen'} >Click Me!</Button>
-            Hover: <Button style={'emptygreen'} hover={true} >Click Me!</Button>
-            Active: <Button style={'emptygreen'} active={true} >Click Me!</Button>
-            Disabled: <Button style={'emptygreen'} disabled={true} >Click Me!</Button>
+            <div style={{display:'flex', alignItems:'center', justifyContent:'space-between'}}>
+            <div>Standard: <Button style={'emptygreen'} >Click Me!</Button></div>
+            <div>Hover: <Button style={'emptygreen'} hover={true} >Click Me!</Button></div>
+            <div>Active: <Button style={'emptygreen'} active={true} >Click Me!</Button></div>
+            <div>Disabled: <Button style={'emptygreen'} disabled={true} >Click Me!</Button></div>
+            </div>
 
             <h4>Style = fullred</h4>
-            Standard: <Button style={'fullred'} >Click Me!</Button>
-            Hover: <Button style={'fullred'} hover={true} >Click Me!</Button>
-            Active: <Button style={'fullred'} active={true} >Click Me!</Button>
-            Disabled: <Button style={'fullred'} disabled={true} >Click Me!</Button>
+            <div style={{display:'flex', alignItems:'center', justifyContent:'space-between'}}>
+            <div>Standard: <Button style={'fullred'} >Click Me!</Button></div>
+            <div>Hover: <Button style={'fullred'} hover={true} >Click Me!</Button></div>
+            <div>Active: <Button style={'fullred'} active={true} >Click Me!</Button></div>
+            <div>Disabled: <Button style={'fullred'} disabled={true} >Click Me!</Button></div>
+            </div>
 
             <h4>Style = emptyred</h4>
-            Standard: <Button style={'emptyred'} >Click Me!</Button>
-            Hover: <Button style={'emptyred'} hover={true} >Click Me!</Button>
-            Active: <Button style={'emptyred'} active={true} >Click Me!</Button>
-            Disabled: <Button style={'emptyred'} disabled={true} >Click Me!</Button>
+            <div style={{display:'flex', alignItems:'center', justifyContent:'space-between'}}>
+            <div>Standard: <Button style={'emptyred'} >Click Me!</Button></div>
+            <div>Hover: <Button style={'emptyred'} hover={true} >Click Me!</Button></div>
+            <div>Active: <Button style={'emptyred'} active={true} >Click Me!</Button></div>
+            <div>Disabled: <Button style={'emptyred'} disabled={true} >Click Me!</Button></div>
+            </div>
 
             <h4>Heights</h4>
-            30: <Button height={30}>Click Me!</Button>
-            36: <Button height={36}>Click Me!</Button>
-            42: <Button height={42}>Click Me!</Button>
+            <div style={{display:'flex', alignItems:'center', justifyContent:'space-between'}}>
+            <div>small: <Button height={'small'}>Click Me!</Button></div>
+            <div>medium: <Button height={'medium'}>Click Me!</Button></div>
+            <div>large: <Button height={'large'}>Click Me!</Button></div>
+            </div>
         </div>
     )
 }
