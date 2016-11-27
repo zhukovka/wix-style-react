c60ed753456b607352e8630c5a0e3a034fb37486
Yaniv Efraim <yaniv.efraim@gmail.com>
2016-11-27 17:04:42 +0200

    added testkit to e2e

--- test/e2e/Button.e2e.js
+++ test/e2e/Button.e2e.js
@@ -1,13 +1,15 @@
-const eyes = require('eyes.it');
+import eyes from 'eyes.it';
+import ButtonDriver from '../../testkit/Button';
 
 describe('Button', () => {
-
+  let driver;
   eyes.it('should have button', () => {
+    driver = new ButtonDriver({id: 'main-example', find: selector => $(selector)});
     browser.get('iframe.html?selectedKind=Components&selectedStory=Button');
     var EC = protractor.ExpectedConditions;
     browser.wait(EC.visibilityOf($('#main-example')), 15000);
 
-    $('#main-example').click(); //TODO: integrate testkit here?
+    driver.click(); //TODO: integrate testkit here?
     expect($('#main-example-label').getText()).toBe('clicked!');
   }, 30000);
 });
