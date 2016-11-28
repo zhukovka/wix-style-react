0b66aa0dad1f2068a71efd4e248b57cf7d25e55b
Yaniv Efraim <yaniv.efraim@gmail.com>
2016-11-29 00:33:51 +0200

    refactor tests

---
+++ src/Button/Button.driver.js
@@ -0,0 +1,57 @@
+import {shallow} from 'enzyme';
+import Button from './Button';
+
+export default class ButtonDriver {
+
+  given = {
+    onClick: fn => {
+      this.onClick = fn;
+      return this;
+    },
+    children: children => {
+      this.children = children;
+      return this;
+    },
+    disabled: disabled => {
+      this.props.disabled = disabled;
+      return this;
+    },
+    height: height => {
+      this.props.height = height;
+      return this;
+    },
+    style: style => {
+      this.props.style = style;
+      return this;
+    },
+    hover: () => {
+      this.props.hover = true;
+      return this;
+    },
+    id: id => {
+      this.id = id;
+      return this;
+    }
+  };
+
+  when = {
+    created: () => {
+      this.wrapper = shallow(
+        <Button id={this.id} onClick={this.onClick} {...this.props}>{this.children}</Button>
+      );
+      return this;
+    },
+    clicked: () => {
+      this.wrapper.simulate('click');
+      return this;
+    }
+  };
+
+  get = {
+    element: () => this.wrapper
+  }
+
+  constructor() {
+    this.props = {};
+  }
+}
--- src/Button/Button.spec.js
+++ src/Button/Button.spec.js
@@ -1,63 +1,79 @@
-import React from 'react';
-import {shallow} from 'enzyme';
-import Button from './Button';
+import 'react';
 import styles from './Button.scss';
-import ButtonDriver from '../../testkit/Button';
+import ButtonDriver from './Button.driver';
 
 describe('Button', () => {
-  const actions = {
-    onClickHandler: jest.fn()
-  }
+  let driver;
 
-  const setup = (props, children) => {
-    const wrapper = shallow(
-      <Button id="my-button" onClick={actions.onClickHandler} {...props}>{children}</Button>
-    );
-    const driver = new ButtonDriver({id: 'my-button', find: selector => wrapper.find(selector)});
-    return {wrapper, driver};
-  };
+  beforeEach(() => {
+    driver = new ButtonDriver();
+  })
 
   it('should click a button', () => {
-    const {driver} = setup();
-    driver.click();
-    expect(actions.onClickHandler).toBeCalled();
+    const onClick = jest.fn();
+
+    driver
+      .given.onClick(onClick)
+      .when.created()
+      .when.clicked();
+
+    expect(onClick).toBeCalled();
   });
 
   it('should render children', () => {
-    const {driver} = setup({}, '<div>123</div>');
-    driver.click();
-    expect(driver.element.text()).toBe('<div>123</div>');
+    driver
+      .given.children('<div>123</div>')
+      .when.created();
+
+    expect(driver.get.element().text()).toBe('<div>123</div>');
   });
 
   it('should get disabled class', () => {
-    const {driver} = setup({disabled: true});
-    expect(driver.element.hasClass(styles.disabled)).toBe(true);
+    driver
+      .given.disabled(true)
+      .when.created();
+
+    expect(driver.get.element().hasClass(styles.disabled)).toBe(true);
   });
 
   it('should have default "fullblue" style', () => {
-    const {driver} = setup();
-    expect(driver.element.hasClass(styles.fullblue)).toBe(true);
+    driver
+      .when.created();
+
+    expect(driver.get.element().hasClass(styles.fullblue)).toBe(true);
   });
 
   it('should get "small" height class', () => {
-    const {driver} = setup({height: `${styles.small}`});
-    expect(driver.element.hasClass(`height${styles.small}`)).toBe(true);
+    driver
+      .given.height(`${styles.small}`)
+      .when.created();
+
+    expect(driver.get.element().hasClass(`height${styles.small}`)).toBe(true);
   });
 
   it('should get "large" height class', () => {
-    const {driver} = setup({height: `${styles.large}`});
-    expect(driver.element.hasClass(`height${styles.large}`)).toBe(true);
+    driver
+      .given.height(`${styles.large}`)
+      .when.created();
+
+    expect(driver.get.element().hasClass(`height${styles.large}`)).toBe(true);
   });
 
   it('should get custom style', () => {
     const customStyle = 'emptyblue';
-    const {driver} = setup({style: customStyle});
-    expect(driver.element.hasClass(styles[customStyle])).toBe(true);
+
+    driver
+      .given.style(customStyle)
+      .when.created();
+
+    expect(driver.get.element().hasClass(styles[customStyle])).toBe(true);
   });
 
   it('should get "hover" class', () => {
-    const {driver} = setup({hover: true});
-    driver.hover();
-    expect(driver.element.hasClass(styles.hover)).toBe(true);
+    driver
+      .given.hover()
+      .when.created();
+
+    expect(driver.get.element().hasClass(styles.hover)).toBe(true);
   });
 });
