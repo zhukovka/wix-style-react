5321386c63e30ee579d7d02ab3edec4c1d08d0f6
Yaniv Efraim <yaniv.efraim@gmail.com>
2016-11-29 10:23:32 +0200

    added tests for Select

---
+++ src/Select/Select.driver.js
@@ -0,0 +1,47 @@
+import {shallow} from 'enzyme';
+import Select from './Select';
+
+export default class SelectDriver {
+
+  given = {
+    options: options => {
+      this.props.options = options;
+      return this;
+    },
+    selectedOption: value => {
+      this.props.value = value;
+      return this;
+    },
+    onChange: fn => {
+      this.onChange = fn;
+      return this;
+    }
+  };
+
+  when = {
+    created: () => {
+      this.wrapper = shallow(
+        <Select {...this.props} onChange={this.onChange}></Select>);
+      return this;
+    },
+    clicked: () => {
+      this.wrapper.find('.wix-select-button').simulate('click');
+      return this;
+    },
+    clickOption: value => {
+      this.wrapper.find('.option').at(value).simulate('click');
+      return this;
+    }
+  };
+
+  get = {
+    element: () => this.wrapper,
+    renderedOptions: () => this.wrapper.find('.option'),
+    content: () => this.wrapper.find('.shown'),
+    selectedContent: () => this.wrapper.find('.buttonText')
+  }
+
+  constructor() {
+    this.props = {};
+  }
+}
---
+++ src/Select/Select.spec.js
@@ -0,0 +1,77 @@
+import SelectDriver from './Select.driver';
+
+describe('Select', () => {
+  let driver;
+  beforeEach(() => {
+    driver = new SelectDriver();
+  });
+
+  it('should render select', () => {
+    const options = [
+      {value:'0', text:'Option 1'},
+      {value:'1', text:'Option 2'},
+      {value:'2', text:'Option 3'}
+    ];
+
+    driver
+      .given.options(options)
+      .when.created();
+
+    expect(driver.get.renderedOptions().length).toBe(options.length);
+
+    options.forEach((option, index) => {
+      expect(driver.get.renderedOptions().at(index).text()).toBe(option.text);
+    });
+  });
+
+  it('should open select on tap', () => {
+    const options = [
+      {value:'0', text:'Option 1'},
+      {value:'1', text:'Option 2'},
+      {value:'2', text:'Option 3'}
+    ];
+
+    driver
+      .given.options(options)
+      .when.created();
+
+    expect(driver.get.content().length).toBe(0);
+
+    driver.when.clicked();
+
+    expect(driver.get.content().length).toBe(1);
+  });
+
+  it('should have initial selected value', () => {
+    const options = [
+      {value:'0', text:'Option 1'},
+      {value:'1', text:'Option 2'},
+      {value:'2', text:'Option 3'}
+    ];
+
+    driver
+      .given.options(options)
+      .given.selectedOption('1')
+      .when.created();
+
+    expect(driver.get.selectedContent().text()).toBe(options[1].text);
+  });
+
+  it('should select value', () => {
+    const onChange = jest.fn(val => driver.get.element().setProps({value: val}));
+    const options = [
+      {value:'0', text:'Option 1'},
+      {value:'1', text:'Option 2'},
+      {value:'2', text:'Option 3'}
+    ];
+
+    driver
+      .given.options(options)
+      .given.onChange(onChange)
+      .when.created()
+      .when.clicked()
+      .when.clickOption(options[2].value);
+
+    expect(driver.get.selectedContent().text()).toBe(options[2].text);
+  });
+});
