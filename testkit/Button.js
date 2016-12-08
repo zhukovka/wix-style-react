7585c1930ed5980237da4c2b46a2d7b54275213e
Yaniv Efraim <yaniv.efraim@gmail.com>
2016-12-08 16:03:03 +0200

    clean testkit

--- testkit/Button.js
+++ testkit/Button.js
@@ -1,26 +1,4 @@
 import {buttonDriverFactory} from '../src/Button/Button.driver';
-export default class ButtonDriver {
-
-  constructor({id, find}) {
-    this.id = id;
-    this.find = find;
-    this._element = find(`#${id}`);
-  }
-
-  get element() {
-    return this._element;
-  }
-
-  click() {
-    this.element.simulate ? this.element.simulate('click') : this.element.click();
-  }
-
-  hover() {
-    if (this.element.simulate) {
-      this.element.simulate('mouseenter');
-    }
-  }
-}
 
 const buttonTestkitFactory = ({wrapper, id}) => {
   //TODO: add support for other wrapper types by checking wrapper type here
