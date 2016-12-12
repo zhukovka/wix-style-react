864460c17da54c445772a8240cfa9cb8e34a36b0
Mantas Miliukas <mantasm@wix.com>
2016-12-12 14:45:47 +0200

    fix: loadash ---> lodash.uniqueid

--- src/ToggleSwitch/ToggleSwitch.js
+++ src/ToggleSwitch/ToggleSwitch.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import classnames from 'classnames';
-import {uniqueId} from 'lodash';
+import uniqueId from 'lodash.uniqueid';
 
 import styles from './ToggleSwitch.scss';
 
