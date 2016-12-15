e73d5f841c2e2c3c88190bb0ee9b8767bf3ee947
Yaniv Efraim <yaniv.efraim@gmail.com>
2016-12-16 01:42:43 +0200

    put back button e2e test

--- src/Button/Button.e2e.js
+++ src/Button/Button.e2e.js
@@ -1,7 +1,17 @@
+import eyes from 'eyes.it';
+import {protractorButtonTestkitFactory} from './testkit/Button.protractor';
 
 describe('Button', () => {
-  it('should click a button', () => {
+  eyes.it('should click a button', () => {
 
-    expect(true).toBe(true);
+    const driver = protractorButtonTestkitFactory({id: 'fullblue'});
+
+    browser.get('iframe.html?selectedKind=3.%20Buttons&selectedStory=3.1%20Standard');
+
+    const EC = protractor.ExpectedConditions;
+    browser.wait(EC.visibilityOf(driver.element()), 15000);
+
+    driver.click();
+    expect(driver.getButtonText()).toBe('clicked!');
   });
 });
