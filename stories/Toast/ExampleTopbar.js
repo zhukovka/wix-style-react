a6edbff57b0d969587a65178c5159ab7fa44aa0b
Ronen Amiel <ronen.amiel@gmail.com>
2016-12-15 17:12:56 +0200

    linting issues for Toast

--- stories/Toast/ExampleTopbar.js
+++ stories/Toast/ExampleTopbar.js
@@ -6,43 +6,43 @@ class ExampleToastToolbar extends React.Component {
 
   constructor(params) {
     super(params);
-    this.state = {showToast: false, theme: 'red', location: 'topfixed', type:'bar'};
+    this.state = {showToast: false, theme: 'red', location: 'topfixed', type: 'bar'};
   }
 
   render() {
 
     return (
-        <div className='ltr'>
+      <div className="ltr">
 
-          <Toast 
-            type={this.state.type}
-            location={this.state.location}
-            theme={this.state.theme} 
-            show={this.state.showToast} 
-            timeout={0} 
-            onClose={() => this.setState({showToast:false})}
-            >
+        <Toast
+          type={this.state.type}
+          location={this.state.location}
+          theme={this.state.theme}
+          show={this.state.showToast}
+          timeout={0}
+          onClose={() => this.setState({showToast: false})}
+          >
 
-            Boo! I scared you with this very scary error message!<Toast.Button onClick={()=>{}}>Thanks</Toast.Button>
+          Boo! I scared you with this very scary error message!<Toast.Button onClick={() => {}}>Thanks</Toast.Button>
 
-          </Toast>
+        </Toast>
 
-          <div style={{display:'flex', justifyContent:'space-between'}}>
-          <Button theme="fullblue" onClick={() => this.setState({theme:'blue', type:'bar', showToast:true})}>Show Toast</Button>
-          <Button theme="fullred" onClick={() => this.setState({theme:'red', type:'bar', showToast: true})}>Show Toast</Button>
-          <Button theme="fullpurple" onClick={() => this.setState({theme:'purple', type:'bar', showToast: true})}>Show Toast</Button>
-          <Button theme="fullgreen" onClick={() => this.setState({theme:'green', type:'bar', showToast: true})}>Show Toast</Button>
-          </div>
-          <br/>
-          <div style={{display:'flex', justifyContent:'space-between'}}>
-          <Button theme="fullblue" onClick={() => this.setState({theme:'blue', type:'largebar', showToast:true})}>Show Toast (large)</Button>
-          <Button theme="fullred" onClick={() => this.setState({theme:'red', type:'largebar', showToast: true})}>Show Toast (large)</Button>
-          <Button theme="fullpurple" onClick={() => this.setState({theme:'purple', type:'largebar', showToast: true})}>Show Toast (large)</Button>
-          <Button theme="fullgreen" onClick={() => this.setState({theme:'green', type:'largebar', showToast: true})}>Show Toast (large)</Button>
-          </div>
-
+        <div style={{display: 'flex', justifyContent: 'space-between'}}>
+          <Button theme="fullblue" onClick={() => this.setState({theme: 'blue', type: 'bar', showToast: true})}>Show Toast</Button>
+          <Button theme="fullred" onClick={() => this.setState({theme: 'red', type: 'bar', showToast: true})}>Show Toast</Button>
+          <Button theme="fullpurple" onClick={() => this.setState({theme: 'purple', type: 'bar', showToast: true})}>Show Toast</Button>
+          <Button theme="fullgreen" onClick={() => this.setState({theme: 'green', type: 'bar', showToast: true})}>Show Toast</Button>
         </div>
-    )
+        <br/>
+        <div style={{display: 'flex', justifyContent: 'space-between'}}>
+          <Button theme="fullblue" onClick={() => this.setState({theme: 'blue', type: 'largebar', showToast: true})}>Show Toast (large)</Button>
+          <Button theme="fullred" onClick={() => this.setState({theme: 'red', type: 'largebar', showToast: true})}>Show Toast (large)</Button>
+          <Button theme="fullpurple" onClick={() => this.setState({theme: 'purple', type: 'largebar', showToast: true})}>Show Toast (large)</Button>
+          <Button theme="fullgreen" onClick={() => this.setState({theme: 'green', type: 'largebar', showToast: true})}>Show Toast (large)</Button>
+        </div>
+
+      </div>
+    );
   }
 }
 
