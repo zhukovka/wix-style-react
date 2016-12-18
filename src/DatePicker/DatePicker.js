88881cd1c8f16ff7320eac6dece314132cef1522
Alon Nachshon <alonn@wix.com>
2016-12-18 14:29:31 +0200

    DatePicker: fix lint issues

--- src/DatePicker/DatePicker.js
+++ src/DatePicker/DatePicker.js
@@ -1,42 +1,37 @@
 import React, {Component} from 'react';
 import ReactDatepicker from 'react-datepicker';
 import DatePickerInput from './DatePickerInput';
-import $ from 'jquery';
 import './DatePicker.scss';
 
 export default class DatePicker extends Component {
-    static propTypes = {
-        style: React.PropTypes.object,
-        value: React.PropTypes.value,
-        onChange: React.PropTypes.func.isRequired
-    };
+  static propTypes = {
+    style: React.PropTypes.object,
+    value: React.PropTypes.value,
+    onChange: React.PropTypes.func.isRequired
+  };
 
-    static defaultProps = {
-        style: {
-            width: 150
-        }
-    };
-
-    constructor(props) {
-        super(props);
+  static defaultProps = {
+    style: {
+      width: 150
     }
+  };
 
-    renderInput() {
-        return <DatePickerInput
-                    style={this.props.style}
-                />;
-    }
+  renderInput() {
+    return (
+      <DatePickerInput style={this.props.style}/>
+    );
+  }
 
-    render() {
-        return (
-            <div className="wix-datepicker">
-                <ReactDatepicker
-                    selected={this.props.value}
-                    onChange={this.props.onChange}
-                    customInput={this.renderInput()}
-                    {...this.props}
-                />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="wix-datepicker">
+        <ReactDatepicker
+          selected={this.props.value}
+          onChange={this.props.onChange}
+          customInput={this.renderInput()}
+          {...this.props}
+          />
+      </div>
+    );
+  }
 }
