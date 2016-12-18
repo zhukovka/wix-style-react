ced1fb015ab999818489444ddaf35dc8895934a2
Ronen Amiel <ronen.amiel@gmail.com>
2016-12-18 16:01:44 +0200

    correct datepicker example to import from 'wix-style-react'

--- stories/DatePicker/ExampleControlled.js
+++ stories/DatePicker/ExampleControlled.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import {DatePicker} from '../../src/index.js';
+import {DatePicker} from 'wix-style-react';
 
 class ControlledDatePicker extends React.Component {
+  constructor(params) {
+    super(params);
 
-    constructor(params) {
-        super(params);
+    this.state = {
+      value: null
+    };
+  }
 
-        this.state = {
-            value: null
-        };
-    }
-
-    render() {
-        return (
-            <DatePicker
-                value={this.state.value}
-                onChange={value => this.setState({value})}
-                dateFormat="DD/MM/YYYY"
-            />
-        );
-    }
+  render() {
+    return (
+      <DatePicker
+        value={this.state.value}
+        onChange={value => this.setState({value})}
+        dateFormat="DD/MM/YYYY"
+        />
+    );
+  }
 }
 
 export default ControlledDatePicker;
