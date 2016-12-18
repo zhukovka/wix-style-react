e3a8d7774e8d24cd3536d0982ba319ae7819d5e4
Alon Nachshon <alonn@wix.com>
2016-12-18 15:04:52 +0200

    DatePicker: input with a calendar icon

--- src/DatePicker/DatePickerInput.js
+++ src/DatePicker/DatePickerInput.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Input from '../Input';
+import styles from './DatePickerInput.scss';
 
 export default class DatePickerInput extends Component {
   static propTypes = {
@@ -9,10 +10,14 @@ export default class DatePickerInput extends Component {
     style: React.PropTypes.object,
   };
 
+  renderIcon() {
+    return <div className={styles.icon}/>;
+  }
+
   render() {
     return (
       <div style={this.props.style} onClick={this.props.onClick}>
-        <Input value={this.props.value}/>
+        <Input value={this.props.value} iconLeft={this.renderIcon()}/>
       </div>
     );
   }
