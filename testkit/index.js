409729845d392090b0944cff1f7253c0eabae341
Yaniv Efraim <yaniv.efraim@gmail.com>
2016-12-20 21:56:00 +0200

    fix testkit reference

--- testkit/index.js
+++ testkit/index.js
@@ -1,4 +1,4 @@
 export {buttonTestkitFactory} from '../src/Button/testkit/Button';
-export {inputDriverFactory} from '../src/Input/Input.driver';
+export {inputDriverFactory} from '../src/Input/testkit/Input';
 export {checkboxDriverFactory} from '../src/Checkbox/Checkbox.driver';
 export {radioGroupDriverFactory} from '../src/RadioGroup/RadioGroup.driver';
