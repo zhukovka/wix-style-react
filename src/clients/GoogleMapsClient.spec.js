52974ea1377122994373f0cea7f8127b17f2cd1c
3LOK <yoava@wix.com>
2016-12-21 11:03:29 +0200

    Fixed lint

--- src/clients/GoogleMapsClient.spec.js
+++ src/clients/GoogleMapsClient.spec.js
@@ -1,5 +1,3 @@
-import {stub} from 'sinon';
-
 import GoogleMapsClient from './GoogleMapsClient';
 
 describe('GoogleMapsClient', () => {
@@ -7,25 +5,25 @@ describe('GoogleMapsClient', () => {
   afterEach(() => delete window.google);
 
   it('should handle null when autocompleting and getting ZERO_RESULTS', () => {
-    window.google = GoogleMapsMock({
+    window.google = new GoogleMapsMock({
       getPlacePredictions: (request, callback) => {
         callback(null, window.google.maps.GeocoderStatus.ZERO_RESULTS);
       }
     });
     const client = new GoogleMapsClient();
-    return client.autocomplete({ request: {} }).then(result => {
+    return client.autocomplete({request: {}}).then(result => {
       expect(result).toEqual([]);
     });
   });
 
   it('should handle null when geocoding and getting ZERO_RESULTS', () => {
-    window.google = GoogleMapsMock(null, {
+    window.google = new GoogleMapsMock(null, {
       geocode: (request, callback) => {
         callback(null, window.google.maps.GeocoderStatus.ZERO_RESULTS);
       }
     });
     const client = new GoogleMapsClient();
-    return client.geocode({ request: {} }).then(result => {
+    return client.geocode({request: {}}).then(result => {
       expect(result).toEqual([]);
     });
   });
