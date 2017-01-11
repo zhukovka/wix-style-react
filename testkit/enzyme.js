04c9ed664d084441d81955617b7f2f8a3908b225
Nir Hartmann <nirh@wix.com>
2017-01-11 16:44:54 +0200

    export dropdown enzyme

--- testkit/enzyme.js
+++ testkit/enzyme.js
@@ -1,2 +1,3 @@
 export {buttonTestkitFactory} from '../src/Button/testkit/Button.enzyme';
 export {toastTestkitFactory} from '../src/Toast/testkit/Toast.enzyme';
+export {dropdownTestkitFactory} from '../src/Dropdown/testKit/Dropdown.enzyme';
