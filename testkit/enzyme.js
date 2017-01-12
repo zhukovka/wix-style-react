5aa1f773929e48cac91bec26cf50b6c4d0f8c794
Nir Hartmann <nirh@wix.com>
2017-01-12 18:07:09 +0200

    Revert "typo"
    This reverts commit d1ee0cc19ba4f1966a8630a663bb36b49582f6a1.

--- testkit/enzyme.js
+++ testkit/enzyme.js
@@ -1,7 +1,7 @@
-import {enzymeTestkitFactoryCreator} from '../src/test-common';
+import {enzymetestkitFactoryCreator} from '../src/test-common';
 
 import inputDriver from '../src/Input/Input.driver';
-export const inputTestkitFactory = enzymeTestkitFactoryCreator(inputDriver);
+export const inputTestkitFactory = enzymetestkitFactoryCreator(inputDriver);
 
 export {buttonTestkitFactory} from '../src/Button/testkit/Button.enzyme';
 export {toastTestkitFactory} from '../src/Toast/testkit/Toast.enzyme';
