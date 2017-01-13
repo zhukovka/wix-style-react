ca4d95f6505e5934eceee4e138c2c1568f355eba
Nir Hartmann <nirh@wix.com>
2017-01-13 17:04:51 +0200

    small fix

--- src/Label/Label.js
+++ src/Label/Label.js
@@ -18,7 +18,7 @@ Label.displayName = 'Label';
 Label.propTypes = {
   id: React.PropTypes.string,
   for: React.PropTypes.string,
-  appearance: React.PropTypes.oneOf(['T1', 'T1.1', 'T1.2', 'T1.3', 'T1.4', 'T2', 'T2.1', 'T2.2', 'T2.3', 'T3', 'T3.1', 'T3.2', 'T3.3', 'T3.4', 'T4', 'T4.1', 'T4.2', 'T4.3', 'T5', 'T5.1']).isRequired,
+  appearance: React.PropTypes.oneOf(['T1', 'T1.1', 'T1.2', 'T1.3', 'T1.4', 'T2', 'T2.1', 'T2.2', 'T2.3', 'T3', 'T3.1', 'T3.2', 'T3.3', 'T3.4', 'T4', 'T4.1', 'T4.2', 'T4.3', 'T5', 'T5.1']),
   children: React.PropTypes.any
 };
 
