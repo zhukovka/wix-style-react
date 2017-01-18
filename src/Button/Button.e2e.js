3f53590846ba48417ce3e6c34764cac7e11a1ec4
Nir Hartmann <nirh@wix.com>
2017-01-18 18:09:16 +0200

    lint

--- src/Button/Button.e2e.js
+++ src/Button/Button.e2e.js
@@ -1,14 +1,14 @@
 import eyes from 'eyes.it';
-import {protractorButtonTestkitFactory} from './testkit/Button.protractor';
+// import {protractorButtonTestkitFactory} from './testkit/Button.protractor';
 
 describe('Button', () => {
   eyes.it('should click a button', () => {
 
-    const driver = protractorButtonTestkitFactory({id: 'button'});
+    // const driver = protractorButtonTestkitFactory({id: 'button'});
 
     browser.get('iframe.html?selectedKind=3.%20Buttons&selectedStory=3.1%20Standard');
 
-    const EC = protractor.ExpectedConditions;
+    // const EC = protractor.ExpectedConditions;
     // browser.wait(EC.visibilityOf(driver.element()), 15000);
 
     // driver.click();
--- src/Label/Label.e2e.js
+++ src/Label/Label.e2e.js
@@ -1,13 +1,13 @@
 import eyes from 'eyes.it';
-import {labelTestkitFactory} from '../../testkit/protractor';
+// import {labelTestkitFactory} from '../../testkit/protractor';
 
 describe('Label', () => {
   eyes.it('should focus on the input when clicked', () => {
-    const driver = labelTestkitFactory({dataHook: 'label'});
+    // const driver = labelTestkitFactory({dataHook: 'label'});
 
     browser.get('iframe.html?selectedKind=7.%20Labels&selectedStory=7.1%20Standard');
 
-    const EC = protractor.ExpectedConditions;
+    // const EC = protractor.ExpectedConditions;
     // browser.wait(EC.visibilityOf(driver.element()), 15000);
 
     // driver.click();
