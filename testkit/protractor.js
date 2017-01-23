28f10c1bf62785e9b77ef36f604a8ed9bd65e1f3
Lior Belinsky <liorbe@wix.com>
2017-01-23 09:54:49 +0200

    Update protractor.js

--- testkit/protractor.js
+++ testkit/protractor.js
@@ -3,5 +3,7 @@ import {protractorTestkitFactoryCreator} from '../src/test-common';
 import labelDriverFactory from '../src/Label/Label.protractor.driver';
 export const labelTestkitFactory = protractorTestkitFactoryCreator(labelDriverFactory);
 
-export {protractorButtonTestkitFactory} from '../src/Button/testkit/Button.protractor';
+import buttonDriverFactory from '../src/Button/Button.protractor.driver';
+export const buttonTestkitFactory = protractorTestkitFactoryCreator(buttonDriverFactory);
+
 export {protractorToastTestkitFactory} from '../src/Toast/testkit/Toast.protractor';
