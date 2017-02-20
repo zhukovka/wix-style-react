15391d52e4ab316419e235dda19970e9448334cc
Nir Hartmann <nirh@wix.com>
2017-02-20 12:37:58 +0200

    fix dropdown example

--- stories/Dropdown/ExampleWithCustomValue.js
+++ stories/Dropdown/ExampleWithCustomValue.js
@@ -10,34 +10,41 @@ const style = {
   marginBottom: '350px'
 };
 
-const customValue = (
-  <div>
-    <span>Custom Value</span>
-    <span style={{marginLeft: '5px'}} onClick={e => e.stopPropagation()}>
-      <Checkbox onChange={() => console.log(`can't check`)}/>
-    </span>
-  </div>
-);
-
-const options = [
-  {id: 1, value: 'Option 1'},
-  {id: 2, value: 'Option 2'},
-  {id: 3, value: 'Option 3'},
-  {id: 4, value: 'Option 4', disabled: true},
-  {id: 5, value: 'Option 5'},
-  {id: 6, value: customValue}
-];
-
-const valueParser = option => (typeof option.value === 'string') ?
-  option.value :
-  option.value.props.children[0].props.children;
-
 class CustomValuesInDropdown extends React.Component {
-  onSelect(option) {
-    console.log('Selected ', valueParser(option));
+
+  constructor(props) {
+    super(props);
+    this.state = {checkboxChecked: false};
   }
 
   render() {
+
+    const onSelect = (option) => console.log('Selected ', valueParser(option));
+
+    const onChange = () => this.setState({checkboxChecked: !this.state.checkboxChecked});
+
+    const valueParser = option => (typeof option.value === 'string') ?
+      option.value :
+      option.value.props.children[0].props.children;
+
+    const customValue = (
+      <div>
+        <span>Custom Value</span>
+        <span style={{marginLeft: '5px'}} onClick={e => e.stopPropagation()}>
+        <Checkbox onChange={onChange} checked={this.state.checkboxChecked}/>
+      </span>
+      </div>
+    );
+
+    const options = [
+      {id: 1, value: 'Option 1'},
+      {id: 2, value: 'Option 2'},
+      {id: 3, value: 'Option 3'},
+      {id: 4, value: 'Option 4', disabled: true},
+      {id: 5, value: 'Option 5'},
+      {id: 6, value: customValue}
+    ];
+
     return (
       <Dropdown
         options={options}
