884d5e23f2dd208cd42cbd3028e8e42522f2c814
Shlomi Toussia-Cohen <shlomitc@wix.com>
2017-02-23 11:01:12 +0200

    Notification - use the correct element to query

--- src/Notification/Notification.driver.js
+++ src/Notification/Notification.driver.js
@@ -9,11 +9,11 @@ const notificationDriverFactory = ({component, wrapper}) => {
   const actionButtonSelector = '[data-hook="notification-cta-button"]';
   const closeButtonSelector = '[data-hook="notification-close-button"]';
 
-  const classExists = className => wrapper.querySelector(notificationWrapperSelector).classList.contains(className);
+  const classExists = className => component.querySelector(notificationWrapperSelector).classList.contains(className);
 
   return {
     exists: () => !!component,
-    visible: () => !!wrapper.querySelector(notificationWrapperSelector),
+    visible: () => !!component.querySelector(notificationWrapperSelector),
     hasTheme: theme => classExists(`${theme}Theme`),
     isStandardNotification: () => classExists('standardTheme'),
     isErrorNotification: () => classExists('errorTheme'),
@@ -21,15 +21,15 @@ const notificationDriverFactory = ({component, wrapper}) => {
     isWarningNotification: () => classExists('warningTheme'),
     isSmallSize: () => classExists('smallSize'),
     isBigSize: () => classExists('bigSize'),
-    getLabelText: () => wrapper.querySelector(labelTextSelector).textContent,
-    hasActionButton: () => !!wrapper.querySelector(actionButtonSelector),
-    getActionButtonText: () => wrapper.querySelector(actionButtonSelector).textContent,
-    hasCloseButton: () => !!wrapper.querySelector('[data-hook="notification-close-button"]'),
+    getLabelText: () => component.querySelector(labelTextSelector).textContent,
+    hasActionButton: () => !!component.querySelector(actionButtonSelector),
+    getActionButtonText: () => component.querySelector(actionButtonSelector).textContent,
+    hasCloseButton: () => !!component.querySelector('[data-hook="notification-close-button"]'),
     isRelativelyPositioned: () => classExists('relativePosition'),
     isAbsolutePositioned: () => classExists('absolutePosition'),
-    clickOnCloseButton: () => ReactTestUtils.Simulate.click(wrapper.querySelector(closeButtonSelector)),
-    clickOnActionButton: () => ReactTestUtils.Simulate.click(wrapper.querySelector(actionButtonSelector)),
-    getZIndex: () => Number(wrapper.querySelector(notificationWrapperSelector).style['z-index']),
+    clickOnCloseButton: () => ReactTestUtils.Simulate.click(component.querySelector(closeButtonSelector)),
+    clickOnActionButton: () => ReactTestUtils.Simulate.click(component.querySelector(actionButtonSelector)),
+    getZIndex: () => Number(component.querySelector(notificationWrapperSelector).style['z-index']),
     setProps: props => {
       ReactDOM.render(<div ref={r => component = r}><Notification {...props}/></div>, wrapper);
     }
