5a2c1f7f34153fe53e973de41ff1d44051305f44
Shlomi Toussia-Cohen <shlomitc@gmail.com>
2017-03-01 21:25:28 +0200

    ButtonLayout - extract the layout to ButtonLayout component (#321)
    * ButtonLayout - extract the layout to ButtonLayout component
    
    * ButtonLayout - liniting

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import styles from './Button.scss';
-import classNames from 'classnames';
 import WixComponent from '../WixComponent';
+import ButtonLayout from './ButtonLayout';
+import omit from 'lodash.omit';
 
 class Button extends WixComponent {
 
@@ -12,51 +13,37 @@ class Button extends WixComponent {
     this.addIcon = this.addIcon.bind(this);
   }
 
-  addIcon(className, icon) {
-    if (icon) {
-      return (
+  addIcon(className, icon, height) {
+    const iconSize = height === 'small' ? '8px' : height === 'medium' ? '12px' : '16px';
+    return (
+      icon ?
         <div className={className}>
-          {React.cloneElement(icon, {
-            size: this.props.height === 'small' ? '8px' : this.props.height === 'medium' ? '12px' : '16px'
-          })}
-        </div>
-      );
-    }
-    return '';
+          {React.cloneElement(icon, {size: iconSize})}
+        </div> :
+        null
+    );
   }
 
   addPrefix() {
-    return this.addIcon(styles.prefix, this.props.prefixIcon);
+    return this.addIcon(styles.prefix, this.props.prefixIcon, this.props.height);
   }
 
   addSuffix() {
-    return this.addIcon(styles.suffix, this.props.suffixIcon);
+    return this.addIcon(styles.suffix, this.props.suffixIcon, this.props.height);
   }
 
   render() {
-    const {theme, hover, active, disabled, height, onClick, children, type} = this.props;
-
-    const className = classNames({
-      [styles.button]: true,
-      [styles[theme]]: true,
-      [styles.hover]: hover,
-      [styles.active]: active,
-      [styles.disabled]: disabled,
-      [styles[`height${height}`]]: height !== 'medium'
-    });
-
-    const _style = {
-      height
-    };
+    const {disabled, onClick, children, type} = this.props;
+    const buttonLayoutProps = omit(this.props, ['id', 'onClick', 'prefixIcon', 'suffixIcon', 'type']);
 
     return (
-      <button className={className} onClick={onClick} style={_style} disabled={disabled} type={type}>
-        <div className={styles.inner}>
+      <ButtonLayout {...buttonLayoutProps}>
+        <button onClick={onClick} disabled={disabled} type={type}>
           {this.addPrefix()}
           {children}
           {this.addSuffix()}
-        </div>
-      </button>
+        </button>
+      </ButtonLayout>
     );
   }
 }
@@ -66,42 +53,17 @@ Button.defaultProps = {
   height: 'medium'
 };
 
-Button.propTypes = {
-  id: React.PropTypes.string,
-  theme: React.PropTypes.oneOf([
-    'transparent',
-    'fullred',
-    'fullgreen',
-    'fullpurple',
-    'emptyred',
-    'emptygreen',
-    'emptybluesecondary',
-    'emptyblue',
-    'emptypurple',
-    'fullblue',
-    'transparentblue',
-    'whiteblue',
-    'whiteblueprimary',
-    'whitebluesecondary',
-    'close-standard',
-    'close-dark',
-    'close-transparent',
-    'icon-greybackground',
-    'icon-standard',
-    'icon-standardsecondary',
-    'icon-white',
-    'icon-whitesecondary'
-  ]),
-  height: React.PropTypes.oneOf(['small', 'medium', 'large']),
-  hover: React.PropTypes.bool,
-  active: React.PropTypes.bool,
-  disabled: React.PropTypes.bool,
-  onClick: React.PropTypes.func,
-  children: React.PropTypes.any,
-  prefixIcon: React.PropTypes.node,
-  suffixIcon: React.PropTypes.node,
-  type: React.PropTypes.string,
-};
+Button.propTypes = Object.assign({},
+  ButtonLayout.propTypes,
+  {
+    id: React.PropTypes.string,
+    onClick: React.PropTypes.func,
+    children: React.PropTypes.any,
+    prefixIcon: React.PropTypes.node,
+    suffixIcon: React.PropTypes.node,
+    type: React.PropTypes.string
+  }
+);
 
 Button.displayName = 'Button';
 
---
+++ src/Button/ButtonLayout.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import styles from './Button.scss';
+import classNames from 'classnames';
+
+const ButtonLayout = props => {
+  const {theme, hover, active, disabled, height, children} = props;
+
+  const className = classNames({
+    [styles.button]: true,
+    [styles[theme]]: true,
+    [styles.hover]: hover,
+    [styles.active]: active,
+    [styles.disabled]: disabled,
+    [styles[`height${height}`]]: height !== 'medium'
+  });
+
+  const _style = {
+    height
+  };
+
+  return React.cloneElement(
+    children,
+    {className, style: _style},
+    <div className={styles.inner}>
+      {children.props.children}
+    </div>
+    );
+};
+
+ButtonLayout.propTypes = {
+  theme: React.PropTypes.oneOf([
+    'transparent',
+    'fullred',
+    'fullgreen',
+    'fullpurple',
+    'emptyred',
+    'emptygreen',
+    'emptybluesecondary',
+    'emptyblue',
+    'emptypurple',
+    'fullblue',
+    'transparentblue',
+    'whiteblue',
+    'whiteblueprimary',
+    'whitebluesecondary',
+    'close-standard',
+    'close-dark',
+    'close-transparent',
+    'icon-greybackground',
+    'icon-standard',
+    'icon-standardsecondary',
+    'icon-white',
+    'icon-whitesecondary'
+  ]),
+  height: React.PropTypes.oneOf(['small', 'medium', 'large']),
+  hover: React.PropTypes.bool,
+  active: React.PropTypes.bool,
+  disabled: React.PropTypes.bool,
+  children: React.PropTypes.any,
+};
+
+ButtonLayout.displayName = 'ButtonLayout';
+
+export default ButtonLayout;
