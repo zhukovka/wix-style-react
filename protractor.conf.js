c6ab34b4abd2565e82b145a9b46ef041bf632fa6
Nir Hartmann <nirh@wix.com>
2017-04-04 10:39:12 +0300

    Temp commit: Force an older version of chormedriver to work with the CI chrome version

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,6 +1,7 @@
 module.exports.config = {
   specs: ['test/**/*.e2e.js', 'src/**/*.e2e.js'],
   baseUrl: `http://localhost:6006/`,
+  chromeDriver: path.resolve('node_modules/chromedriver/bin/chromedriver'),
 
   onPrepare() {
     browser.ignoreSynchronization = true;
