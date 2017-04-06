64e5c9bed8d466535a69e6a5ac51038feb3ef7c6
Nir Hartmann <nirh@wix.com>
2017-04-06 09:00:54 +0300

    no message

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,6 +1,6 @@
 module.exports.config = {
   specs: ['test/**/*.e2e.js', 'src/**/*.e2e.js'],
-  baseUrl: `http://localhost:6006/`
+  baseUrl: `http://localhost:6006/`,
 
   onPrepare() {
     browser.ignoreSynchronization = true;
