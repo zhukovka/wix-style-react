ddddf50e997ad51a092ebc18692e4c40447135a8
Nir Hartmann <nirh@wix.com>
2017-04-06 08:55:54 +0300

    remove temp chromeDriver version fix, its fixed in Yoshi now

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,9 +1,6 @@
-const path = require('path');
-
 module.exports.config = {
   specs: ['test/**/*.e2e.js', 'src/**/*.e2e.js'],
-  baseUrl: `http://localhost:6006/`,
-  chromeDriver: path.resolve('node_modules/chromedriver/bin/chromedriver'),
+  baseUrl: `http://localhost:6006/`
 
   onPrepare() {
     browser.ignoreSynchronization = true;
