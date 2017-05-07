cffeb3c8bdb81fddf5a0e4432b008e0d6ae6c1d6
liorur <lioru@wix.com>
2017-05-07 16:06:45 +0300

    expose additional bootstrap grid options (#433)
    * expose additional bootstrap grid options
    
    * fix lint issues

--- src/Grid/Grid.js
+++ src/Grid/Grid.js
@@ -2,11 +2,12 @@ import React, {Component} from 'react';
 import styles from './Grid.scss';
 import Card from '../Card';
 import classNames from 'classnames';
+import PropTypes from 'prop-types';
 
 class Container extends Component {
 
   static propTypes = {
-    children: React.PropTypes.node
+    children: PropTypes.node
   };
 
   render() {
@@ -21,9 +22,9 @@ class Container extends Component {
 class Row extends Component {
 
   static propTypes = {
-    children: React.PropTypes.node,
-    rtl: React.PropTypes.bool,
-    stretchViewsVertically: React.PropTypes.bool
+    children: PropTypes.node,
+    rtl: PropTypes.bool,
+    stretchViewsVertically: PropTypes.bool
   };
 
   static defaultProps = {
@@ -48,7 +49,7 @@ class AutoAdjustedRow extends Component {
 
   DEFAULT_MAX_SPAN = 12;
   static propTypes = {
-    children: React.PropTypes.node
+    children: PropTypes.node
   };
 
   render() {
@@ -67,17 +68,58 @@ class AutoAdjustedRow extends Component {
 class Col extends Component {
 
   static propTypes = {
-    children: React.PropTypes.node,
-    span: React.PropTypes.number.isRequired,
-    rtl: React.PropTypes.bool
+    children: PropTypes.node,
+    span: PropTypes.number,
+    rtl: PropTypes.bool,
+    xs: PropTypes.number,
+    sm: PropTypes.number,
+    md: PropTypes.number,
+    lg: PropTypes.number,
+    xl: PropTypes.number,
+    hiddenXs: PropTypes.string,
+    hiddenSm: PropTypes.string,
+    hiddenMd: PropTypes.string,
+    hiddenLg: PropTypes.string,
+    hiddenXl: PropTypes.string,
   };
+
+  isUpDown(str) {
+    if (!str) {
+      return false;
+    }
+    str = str.toLowerCase();
+    return str.toLowerCase() === 'up' || str === 'down';
+  }
+
+  isLegalCol(numStr) {
+    if (!numStr) {
+      return false;
+    }
+    const num = Number(numStr);
+    return Number.isInteger(num) && num > 0 && num <= 12;
+  }
+
+  capitilizeFirstLetter(str) {
+    console.log(str);
+    return str ? str.toLowerCase().replace(/(^| )(\w)/g, s => s.toUpperCase()) : str;
+  }
 
   render() {
     const columnClasses = classNames(
       styles.column,
-      styles[`colXs${this.props.span}`], {
-        [styles.rtl]: this.props.rtl
-      });
+      {[styles.rtl]: this.props.rtl},
+      {[styles[`colXs${this.props.span}`]]: this.isLegalCol(this.props.span)},
+      {[styles[`colXs${this.props.xs}`]]: this.isLegalCol(this.props.xs)},
+      {[styles[`colSm${this.props.sm}`]]: this.isLegalCol(this.props.sm)},
+      {[styles[`colMd${this.props.md}`]]: this.isLegalCol(this.props.md)},
+      {[styles[`colLg${this.props.lg}`]]: this.isLegalCol(this.props.lg)},
+      {[styles[`colXl${this.props.xl}`]]: this.isLegalCol(this.props.xl)},
+      {[styles[`hiddenXs` + this.capitilizeFirstLetter(this.props.hiddenXs)]]: this.isUpDown(this.props.hiddenXs)},
+      {[styles[`hiddenSm` + this.capitilizeFirstLetter(this.props.hiddenSm)]]: this.isUpDown(this.props.hiddenSm)},
+      {[styles[`hiddenMd` + this.capitilizeFirstLetter(this.props.hiddenMd)]]: this.isUpDown(this.props.hiddenMd)},
+      {[styles[`hiddenLg` + this.capitilizeFirstLetter(this.props.hiddenLg)]]: this.isUpDown(this.props.hiddenLg)},
+      {[styles[`hiddenXl` + this.capitilizeFirstLetter(this.props.hiddenXl)]]: this.isUpDown(this.props.hiddenXl)}
+    );
 
     return (
       <div className={columnClasses}>
---
+++ src/Grid/Grid.spec.js
@@ -0,0 +1,72 @@
+/**
+ * Created by lioru on 05/05/2017.
+ */
+import React from 'react';
+import {mount} from 'enzyme';
+import {Col} from './Grid';
+
+describe('Grid', () => {
+
+  describe('breakpoints col options', () => {
+    it('should add col xs class when using span attribute ', () => {
+      const element = mount(<Col span="6"/>);
+      expect(element.hasClass('colXs6')).toEqual(true);
+    });
+
+    it('should add col sm class when using sm attribute ', () => {
+      const element = mount(<Col sm="3"/>);
+      expect(element.hasClass('colSm3')).toEqual(true);
+    });
+
+    it('should add col md class when using md attribute ', () => {
+      const element = mount(<Col md="12"/>);
+      expect(element.hasClass('colMd12')).toEqual(true);
+    });
+
+    it('should add col lg class when using lg attribute ', () => {
+      const element = mount(<Col lg="1"/>);
+      expect(element.hasClass('colLg1')).toEqual(true);
+    });
+
+    it('should add col xl class when using xl attribute ', () => {
+      const element = mount(<Col xl="5"/>);
+      expect(element.hasClass('colXl5')).toEqual(true);
+    });
+
+    it('should add col xl class when using xl attribute ', () => {
+      const element = mount(<Col sm="3" md="12" lg="1" xl="5"/>);
+      expect(element.hasClass('colXl5')).toEqual(true);
+      expect(element.hasClass('colLg1')).toEqual(true);
+      expect(element.hasClass('colMd12')).toEqual(true);
+      expect(element.hasClass('colSm3')).toEqual(true);
+    });
+  });
+
+  describe('breakpoints hide options', () => {
+    it('should add hidden xs class when using hiddenxs attribute ', () => {
+      const element = mount(<Col hiddenXs="up"/>);
+      expect(element.hasClass('hiddenXsUp')).toEqual(true);
+    });
+
+    it('should add hidden sm class when using hiddensm attribute ', () => {
+      const element = mount(<Col hiddenSm="down"/>);
+      expect(element.hasClass('hiddenSmDown')).toEqual(true);
+    });
+
+    it('should add hidden md class when using hiddenmd attribute ', () => {
+      const element = mount(<Col hiddenMd="up"/>);
+      expect(element.hasClass('hiddenMdUp')).toEqual(true);
+    });
+
+    it('should add hidden lg class when using hiddenlg attribute ', () => {
+      const element = mount(<Col hiddenLg="Down"/>);
+      expect(element.hasClass('hiddenLgDown')).toEqual(true);
+    });
+
+    it('should add hidden xl class when using hiddenxl attribute ', () => {
+      const element = mount(<Col hiddenXl="up"/>);
+      expect(element.hasClass('hiddenXlUp')).toEqual(true);
+    });
+  });
+});
+
