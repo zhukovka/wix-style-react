f49df1fc8f278b16316d3e7e3c13f7b87ec780dd
Lior Belinsky <liorbe@wix.com>
2017-05-26 13:45:48 +0300

    align text of tooltip in inputArea error message

--- src/InputArea/Exclamation.js
+++ src/InputArea/Exclamation.js
@@ -6,7 +6,12 @@ import {Error} from '../Icons/dist';
 
 const Exclamation = ({errorMessage}) =>
   <div className={styles.suffix}>
-    <Tooltip disabled={errorMessage.length === 0} active={errorMessage.length > 0} placement="right" alignment="center" hideDelay={100} content={errorMessage} moveBy={{x: 0, y: -10}} overlay="">
+    <Tooltip
+      disabled={errorMessage.length === 0} active={errorMessage.length > 0}
+      placement="right" alignment="center" hideDelay={100} content={errorMessage}
+      moveBy={{x: 0, y: -10}} overlay=""
+      textAlign="left"
+      >
       <div className={styles.errorIcon}><Error size="1.5em"/></div>
     </Tooltip>
   </div>;
