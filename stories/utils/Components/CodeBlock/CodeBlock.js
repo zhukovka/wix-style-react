e75e1f4a0bcd48b1ce24148f15b759f73c8192c0
Nir Hartmann <nirh@wix.com>
2017-06-20 10:41:19 +0300

    replace deprecated toast with Notification

--- stories/utils/Components/CodeBlock/CodeBlock.js
+++ stories/utils/Components/CodeBlock/CodeBlock.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import TextButton from '../TextButton';
 import copy from 'copy-to-clipboard';
 import Markdown from '../Markdown';
-import Toast from '../../../../src/Toast';
+import Notification from '../../../../src/Notification';
 
 const toCodeBlock = (code, type = 'js') =>
   ['```' + type, code.trim(), '```'].join('\n');
@@ -21,24 +21,31 @@ export default class CodeBlock extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {showToast: false};
+    this.state = {showNotification: false};
   }
 
   render() {
     const {source, type} = this.props;
     const copyToClipboardClicked = () => {
       copy(source);
-      this.setState({showToast: true});
+      this.setState({showNotification: true});
     };
     return (
       <div>
-        <Toast
-          show={this.state.showToast}
-          timeout={2000}
-          theme="blue"
-          onClose={() => this.setState({showToast: false})}>
-          Copied!
-        </Toast>
+        <Notification
+          onClose={() => this.setState({showNotification: false})}
+          show={this.state.showNotification}
+          size="small"
+          theme="standard"
+          timeout={3000}
+          type="sticky"
+          zIndex={10000}
+          >
+          <Notification.TextLabel>
+            Copied!
+          </Notification.TextLabel>
+          <Notification.CloseButton />
+        </Notification>
 
         <TextButton onClick={copyToClipboardClicked}>Copy to clipboard</TextButton>
         <Markdown source={toCodeBlock(source, type)}/>
