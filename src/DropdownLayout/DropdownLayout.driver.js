ee82db48fb85cfc80444e0e8583668358ef84d2c
Moshe Matsliah <moshem@wix.com>
2017-08-28 18:34:34 +0300

    fix dropdownLayoutDriver doIfOptionExists gets a callback instead of a value (#757)

--- src/DropdownLayout/DropdownLayout.driver.js
+++ src/DropdownLayout/DropdownLayout.driver.js
@@ -15,7 +15,7 @@ const dropdownLayoutDriverFactory = ({element, wrapper, component}) => {
     if (optionsLength() <= position) {
       throw `index out of bounds, try to get option ${position} while only ${optionsLength()} options exists`;
     }
-    return onSuccess;
+    return onSuccess();
   };
 
   return {
@@ -26,27 +26,27 @@ const dropdownLayoutDriverFactory = ({element, wrapper, component}) => {
     hasTheme: theme => isClassExists(element, `theme-${theme}`),
     tabIndex: () => element.tabIndex,
     optionsLength: () => optionsLength(),
-    mouseEnterAtOption: position => doIfOptionExists(position, ReactTestUtils.Simulate.mouseEnter(optionAt(position))),
-    mouseLeaveAtOption: position => doIfOptionExists(position, ReactTestUtils.Simulate.mouseLeave(optionAt(position))),
+    mouseEnterAtOption: position => doIfOptionExists(position, () => ReactTestUtils.Simulate.mouseEnter(optionAt(position))),
+    mouseLeaveAtOption: position => doIfOptionExists(position, () => ReactTestUtils.Simulate.mouseLeave(optionAt(position))),
     mouseClickOutside: () => ReactTestUtils.Simulate.blur(contentContainer),
     isOptionExists: optionText => [].filter.call(options.childNodes, opt => opt.textContent === optionText).length > 0,
-    isOptionHovered: position => doIfOptionExists(position, isClassExists(optionAt(position), 'hovered')),
-    isOptionSelected: position => doIfOptionExists(position, isClassExists(optionAt(position), 'selected')),
-    isOptionHoveredWithGlobalClassName: position => doIfOptionExists(position, isClassExists(optionAt(position), 'wixstylereactHovered')),
-    isOptionSelectedWithGlobalClassName: position => doIfOptionExists(position, isClassExists(optionAt(position), 'wixstylereactSelected')),
+    isOptionHovered: position => doIfOptionExists(position, () => isClassExists(optionAt(position), 'hovered')),
+    isOptionSelected: position => doIfOptionExists(position, () => isClassExists(optionAt(position), 'selected')),
+    isOptionHoveredWithGlobalClassName: position => doIfOptionExists(position, () => isClassExists(optionAt(position), 'wixstylereactHovered')),
+    isOptionSelectedWithGlobalClassName: position => doIfOptionExists(position, () => isClassExists(optionAt(position), 'wixstylereactSelected')),
     classes: () => options.className,
     pressDownKey: () => ReactTestUtils.Simulate.keyDown(element, {key: 'ArrowDown'}),
     pressUpKey: () => ReactTestUtils.Simulate.keyDown(element, {key: 'ArrowUp'}),
     pressEnterKey: () => ReactTestUtils.Simulate.keyDown(element, {key: 'Enter'}),
     pressTabKey: () => ReactTestUtils.Simulate.keyDown(element, {key: 'Tab'}),
     pressEscKey: () => ReactTestUtils.Simulate.keyDown(element, {key: 'Escape'}),
-    optionContentAt: position => doIfOptionExists(position, optionAt(position).textContent),
-    clickAtOption: position => doIfOptionExists(position, ReactTestUtils.Simulate.click(optionAt(position))),
+    optionContentAt: position => doIfOptionExists(position, () => optionAt(position).textContent),
+    clickAtOption: position => doIfOptionExists(position, () => ReactTestUtils.Simulate.click(optionAt(position))),
     clickAtOptionWithValue: value => {
       const option = values(options.childNodes).find(option => option.innerHTML === value);
       option && ReactTestUtils.Simulate.click(option);
     },
-    isOptionADivider: position => doIfOptionExists(position, isClassExists(optionAt(position), 'divider')),
+    isOptionADivider: position => doIfOptionExists(position, () => isClassExists(optionAt(position), 'divider')),
     setProps: props => {
       const ClonedWithProps = React.cloneElement(component, Object.assign({}, component.props, props), ...(component.props.children || []));
       ReactDOM.render(<div ref={r => element = r}>{ClonedWithProps}</div>, wrapper);
