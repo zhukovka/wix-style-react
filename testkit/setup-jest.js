abe2ce0ba57555ff40a9cbb65903c8eec7512d37
Nir Hartmann <nirh@wix.com>
2017-09-10 14:18:36 +0300

    fail test on errors (#808)

---
+++ testkit/setup-jest.js
@@ -0,0 +1,17 @@
+const originConsoleError = console.error;
+let errors = [];
+
+console.error = function (...args) {
+  errors.push(args);
+  originConsoleError.apply(console, args);
+};
+
+beforeEach(() => {
+  errors = [];
+});
+
+afterEach(() => {
+  if (errors.length > 0) {
+    throw new Error(`Test has ${errors.length} Unhandled Errors:\n${errors}`);
+  }
+});
