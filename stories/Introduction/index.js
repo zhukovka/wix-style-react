29c27a4599282727c89fa69f25972907e0b7a694
argshook <argshook@gmail.com>
2017-09-25 14:37:03 +0300

    fix(lint)
    oops :(

--- stories/Introduction/index.js
+++ stories/Introduction/index.js
@@ -9,9 +9,9 @@ import AutoDocsReadme from '../../docs/AutoDocs.md';
 import UsageWithoutYoshiReadme from '../../docs/usage-without-yoshi.md';
 
 storiesOf('Introduction', {})
-  .add('Getting started', () => <Markdown source={Readme} />)
-  .add('Testing', () => <Markdown source={TestsReadme} />)
-  .add('Contribution', () => <Markdown source={Contribution} />)
-  .add('TPA', () => <Markdown source={TPA} />)
-  .add('AutoDocs', () => <Markdown source={AutoDocsReadme} />)
-  .add('Usage Without Yoshi', () => <Markdown source={UsageWithoutYoshiReadme} />);
+  .add('Getting started', () => <Markdown source={Readme}/>)
+  .add('Testing', () => <Markdown source={TestsReadme}/>)
+  .add('Contribution', () => <Markdown source={Contribution}/>)
+  .add('TPA', () => <Markdown source={TPA}/>)
+  .add('AutoDocs', () => <Markdown source={AutoDocsReadme}/>)
+  .add('Usage Without Yoshi', () => <Markdown source={UsageWithoutYoshiReadme}/>);
