73260c8ef84cbff48ee28ece0a7ac2673ee7f729
Arijus Šukys <argshook@gmail.com>
2017-09-28 09:50:55 +0300

    fix/870: run build if dist folder missing whilst npm start (#871)
    closes 780
    
    shell script is to avoid ugly oneliner within package.json scripts

---
+++ scripts/npm-start.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if ! test -d dist; then
+  echo "Welcome to Wix Style React!"
+  echo ""
+  echo "allow me to build project and prepare storybook for first run..."
+  echo ""
+
+  npm run build
+fi
+
+svg2react-icon-once
+yoshi start & npm run storybook
