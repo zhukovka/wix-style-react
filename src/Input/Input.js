b4790d178e2c3d2dd7ef57ac5087ccacd134e195
Arijus Šukys <argshook@gmail.com>
2017-10-19 18:32:48 +0300

    fix(Input): `type` should be string, not node (#1010)
    it seems like copypasta mistake

--- src/Input/Input.js
+++ src/Input/Input.js
@@ -335,7 +335,7 @@ Input.propTypes = {
 
   /** Placement of the error and help tooltips (supported only for amaterial them for now) */
   tooltipPlacement: PropTypes.string,
-  type: PropTypes.node,
+  type: PropTypes.string,
   unit: PropTypes.string,
 
   /** Inputs value */
