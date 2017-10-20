8f0aaea9550de2561ea837f05d6f2da284d00188
argshook <argshook@gmail.com>
2017-10-20 16:36:00 +0300

    test(export-components): ignore
    this test is not entirely accurate.
    we test exported files against `/src/index.js` which includes a
    different set of exports.
    also, it tends to be problematic for people who try to run `npm test`
    the very first time (hint: it fails). They need to run `npm start` at
    least once, but then the root folder is littered with almost empty
    files. which is annoying.
    
    hence, until we figure out what and how we want to export, i'm ignoring
    this test for now.

--- test/export-components.spec.js
+++ test/export-components.spec.js
@@ -20,7 +20,7 @@ const componentsToSkip = [
   'Autocomplete' // special case, a typo in component name, should be AutoComplete
 ];
 
-describe('export components', () => {
+xdescribe('export components', () => {
   it('should contain all components found in index.js', () => {
     const componentsToCheck = Object.keys(exportedComponents).filter(name => componentsToSkip.indexOf(name) === -1);
 
