54eb89bd12315b17c793c2b777ba76fe90b49423
Nikita Shulipa <mykytas@wix.com>
2017-10-25 02:24:49 +0300

    Migrate Checkbox story to new "story()" (#1035)

--- stories/Checkbox/index.js
+++ stories/Checkbox/index.js
@@ -1,13 +1,6 @@
 import React from 'react';
-import {storiesOf} from '@storybook/react';
-import Markdown from '../utils/Components/Markdown';
+import story from '../utils/Components/Story';
 import CodeExample from '../utils/Components/CodeExample';
-
-import CheckboxSource from '!raw-loader!../../src/Checkbox/Checkbox';
-import AutoDocs from '../utils/Components/AutoDocs';
-
-import TabbedView from '../utils/Components/TabbedView';
-import ReadmeTestKit from '../../src/Checkbox/README.TESTKIT.md';
 
 import ExampleStandard from './ExampleStandard';
 import ExampleStandardRaw from '!raw-loader!./ExampleStandard';
@@ -21,25 +14,28 @@ import ExampleRtlRaw from '!raw-loader!./ExampleRtl';
 import ExampleControlled from './ExampleControlled';
 import ExampleControlledRaw from '!raw-loader!./ExampleControlled';
 
-storiesOf('Core', module)
-  .add('Checkbox', () => (
-    <TabbedView tabs={['API', 'TestKits']}>
-      <div>
-        <AutoDocs source={CheckboxSource}/>
-        <h1>Usage examples</h1>
-        <CodeExample title="Standard" code={ExampleStandardRaw}>
-          <ExampleStandard/>
-        </CodeExample>
-        <CodeExample title="Indeterminate" code={ExampleIndeterminateRaw}>
-          <ExampleIndeterminate/>
-        </CodeExample>
-        <CodeExample title="Rtl" code={ExampleRtlRaw}>
-          <ExampleRtl/>
-        </CodeExample>
-        <CodeExample title="Controlled input" code={ExampleControlledRaw}>
-          <ExampleControlled/>
-        </CodeExample>
-      </div>
-      <Markdown source={ReadmeTestKit}/>
-    </TabbedView>
-  ));
+story({
+  category: 'Core',
+  componentSrcFolder: 'Checkbox',
+  componentProps: {
+    children: 'Some text'
+  },
+  examples: (
+    <div>
+      <h1>Usage examples</h1>
+      <CodeExample title="Standard" code={ExampleStandardRaw}>
+        <ExampleStandard/>
+      </CodeExample>
+      <CodeExample title="Indeterminate" code={ExampleIndeterminateRaw}>
+        <ExampleIndeterminate/>
+      </CodeExample>
+      <CodeExample title="Rtl" code={ExampleRtlRaw}>
+        <ExampleRtl/>
+      </CodeExample>
+      <CodeExample title="Controlled input" code={ExampleControlledRaw}>
+        <ExampleControlled/>
+      </CodeExample>
+    </div>
+  )
+});
+
