60c2abe30bcb333ec0db9838514bb1621e0abe17
Nikita Shulipa <mykytas@wix.com>
2017-10-25 02:27:12 +0300

    Migrate Label component to new self-documenting system (#1034)

--- stories/Label/index.js
+++ stories/Label/index.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import {storiesOf} from '@storybook/react';
-import Markdown from '../utils/Components/Markdown';
-import AutoDocs from '../utils/Components/AutoDocs';
+import story from '../utils/Components/Story';
 import CodeExample from '../utils/Components/CodeExample';
-import LabelSource from '!raw-loader!../../src/Label/Label';
-import TabbedView from '../utils/Components/TabbedView';
-import ReadmeTestKit from '../../src/Label/README.TESTKIT.md';
 
 import ExampleStandard from './ExampleStandard';
 import ExampleStandardRaw from '!raw-loader!./ExampleStandard';
 
-storiesOf('Core', module)
-  .add('Label', () => (
-    <TabbedView tabs={['API', 'TestKits']}>
-      <div>
-        <AutoDocs source={LabelSource}/>
-        <h1>Usage examples</h1>
-        <CodeExample title="Standard" code={ExampleStandardRaw}>
-          <ExampleStandard/>
-        </CodeExample>
-      </div>
-      <Markdown source={ReadmeTestKit}/>
-    </TabbedView>
-  ));
+story({
+  category: 'Core',
+  componentSrcFolder: 'Label',
+  componentProps: {
+    children: 'Label text'
+  },
+  examples: (
+    <CodeExample title="Standard" code={ExampleStandardRaw}>
+      <ExampleStandard/>
+    </CodeExample>
+  )
+});
