34f378f4ac2418af0cc9d23401c3a7a62e147212
Arijus Šukys <argshook@gmail.com>
2017-11-09 11:41:29 +0200

    feat(npm run release): naive manual release (#1096)
    * feat(npm run release): naive manual release
    
    bump version manually to trigger ci release
    
    * chore(release.js): fix to let CI publish
    
    CI publishes packages that don't have package.json with private: true.
    
    we become private to not be published when current.version < published.version
    otherwise we become public again, and CI publishes as usual
    
    * chore(release.js): cleaner log messages

---
+++ scripts/release.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+
+if (!process.env.IS_BUILD_AGENT) {
+  console.log('Package will not be published because we\'re not running in a CI build agent');
+  return process.exit(0);
+}
+
+const semver = require('semver');
+const {exec} = require('child_process');
+const fs = require('fs');
+
+const packageJSONPath = '../package.json';
+const packageJSON = require(packageJSONPath);
+
+promisify(fs.writeFile)(
+  packageJSONPath,
+  JSON.stringify(Object.assign({}, packageJSON, {private: true}))
+)
+
+  .then(() => promisify(exec)('npm view wix-style-react version --registry=https://registry.npmjs.org/'))
+
+  .then(publishedVersion => semver.gt(packageJSON.version, publishedVersion))
+
+  .then(shouldPublish => shouldPublish ?
+    promisify(fs.writeFile)(packageJSONPath, packageJSON)
+      .then(() => 'Package will be published because of a version bump in package.json') :
+
+    'Package will not be published because this version is already published'
+  )
+
+  .then(console.log)
+
+  .catch(error => {
+    console.error('ERROR: Unable to publish', error);
+    process.exit(1);
+  });
+
+
+
+// promisify : Function -> List a -> Promise
+function promisify(fn) {
+  return (...args) =>
+    new Promise((resolve, reject) =>
+      fn(
+        ...args,
+        (err, payload) => err ? reject(err) : resolve(payload)
+      )
+    );
+}
