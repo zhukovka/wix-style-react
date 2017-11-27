1988edd099c54f958cffd87ec8e1c609f5b0e684
Lior Belinsky <liorbe@wix.com>
2017-11-27 14:39:43 +0200

    remove tests for unused TpaTextlink component (#1194)

--- src/TPA/TextLink/TextLink.e2e.js
+++ src/TPA/TextLink/TextLink.e2e.js
@@ -1,22 +1,22 @@
-import eyes from 'eyes.it';
-import {tpaTextLinkTestkitFactory, getStoryUrl, waitForVisibilityOf} from '../../../testkit/protractor';
+// import eyes from 'eyes.it';
+// import {tpaTextLinkTestkitFactory, getStoryUrl, waitForVisibilityOf} from '../../../testkit/protractor';
 
-describe('TPA TextLink', () => {
-  const storyUrl = getStoryUrl('TPA', 'TextLink');
-  const linkText = 'Click me';
-  const expectedUrlAfterClick = 'https://wix.github.io/wix-style-react/?selectedKind=Introduction&selectedStory=Getting%20started&full=0&down=0&left=1&panelRight=0';
+// describe('TPA TextLink', () => {
+//   const storyUrl = getStoryUrl('TPA', 'TextLink');
+//   const linkText = 'Click me';
+//   const expectedUrlAfterClick = 'https://wix.github.io/wix-style-react/?selectedKind=Introduction&selectedStory=Getting%20started&full=0&down=0&left=1&panelRight=0';
 
-  eyes.it('should nav to new address after click', () => {
-    const dataHook = 'story-text-link';
-    const driver = tpaTextLinkTestkitFactory({dataHook});
+//   eyes.it('should nav to new address after click', () => {
+//     const dataHook = 'story-text-link';
+//     const driver = tpaTextLinkTestkitFactory({dataHook});
 
-    browser.get(storyUrl);
+//     browser.get(storyUrl);
 
-    waitForVisibilityOf(driver.element(), 'Cannot find text link')
-      .then(() => {
-        expect(driver.getTextContent()).toBe(linkText);
-        driver.click();
-        expect(browser.getCurrentUrl()).toBe(expectedUrlAfterClick);
-      });
-  });
-});
+//     waitForVisibilityOf(driver.element(), 'Cannot find text link')
+//       .then(() => {
+//         expect(driver.getTextContent()).toBe(linkText);
+//         driver.click();
+//         expect(browser.getCurrentUrl()).toBe(expectedUrlAfterClick);
+//       });
+//   });
+// });
