0d9fa3eede60ab55d197d3c9aed220c557e89b5d
Arnoldas Šeščila <arnoldass@wix.com>
2017-12-18 09:23:56 +0200

    Driver files should be accessible in runtime (#1266)
    do not filter out filenames containing `driver` for `rawContextualImport`

--- stories/create-story.js
+++ stories/create-story.js
@@ -4,7 +4,7 @@ import story from 'wix-storybook-utils/Story';
 // yes, there is duplication
 // no, you can't DRY it, webpack parses `require.context` as-is, meaning no dynamic parts allowed
 const contextualImport = require.context('../src', true, /^((?!test-common|assets|providers|spec|e2e|driver|protractor).)+$/);
-const rawContextualImport = require.context('!raw-loader!../src', true, /^((?!test-common|assets|providers|spec|e2e|driver|protractor).)+$/);
+const rawContextualImport = require.context('!raw-loader!../src', true, /^((?!test-common|assets|providers|spec|e2e|protractor).)+$/);
 
 const importWith = importer => path =>
   new Promise(resolve => resolve(importer(path)));
