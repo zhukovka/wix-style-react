13aac294f88096c831373535bd33a538fd0a20c6
Amir Yonatan <amiry@wix.com>
2018-03-25 14:46:37 +0300

    remove 'only

--- src/Page/Page.spec.js
+++ src/Page/Page.spec.js
@@ -58,7 +58,7 @@ describe('Page', () => {
       expect(driver.gradientContainerHeight()).toBe('39px');
     });
 
-    it.only('should not render 0 when maximized but header height delta is 0', () => {
+    it('should not render 0 when maximized but header height delta is 0', () => {
       const driver = createDriver(renderPageWithProps());
       expect(driver.getPageHtml()).not.toContain('>0<');
     });
