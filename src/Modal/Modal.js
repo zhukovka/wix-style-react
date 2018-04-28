cbe436c4a4978254a7bd506828baa78122b2098c
shypeleg <shy.peleg@gmail.com>
2018-04-28 13:59:16 +0300

    Modal - Move box-shadow to each modal layout (#1697)

--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -97,7 +97,6 @@ class Modal extends WixComponent {
         outline: 'none',
         borderRadius,
         padding: '0px',
-        boxShadow: '0 0 14px 0 rgba(22, 45, 60, 0.3)',
         // Overriding defaults - END
         backgroundColor: 'transparent',
         marginBottom: '0px',
