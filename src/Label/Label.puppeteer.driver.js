575da439b74156be5db993787290aeeb9c08d160
Yaniv Efraim <yaniv.efraim@gmail.com>
2018-05-08 19:21:40 +0300

    fix: puppeteer label testkit driver (#1726)

--- src/Label/Label.puppeteer.driver.js
+++ src/Label/Label.puppeteer.driver.js
@@ -1,6 +1,6 @@
 const labelDriverFactory = (component, page) => ({
   click: () => component.click(),
-  getLabelText: () => page.eval(_component => _component.innerText, component),
+  getLabelText: () => page.evaluate(_component => _component.innerText, component),
   element: () => component
 });
 
