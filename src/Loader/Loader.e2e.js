e71ed6c4d072ac6e91c6dcd303f3f16f104e2dc3
Juste Noreikaite <37296556+justenor@users.noreply.github.com>
2018-07-03 08:13:32 +0300

    Loader e2e tests (#1919)
    * Loader e2e tests
    
    * Update Loader.e2e.js

--- src/Loader/Loader.e2e.js
+++ src/Loader/Loader.e2e.js
@@ -1,3 +1,4 @@
+import eyes from 'eyes.it';
 import {getStoryUrl, loaderTestkitFactory, waitForVisibilityOf} from '../../testkit/protractor';
 import autoExampleDriver from 'wix-storybook-utils/AutoExampleDriver';
 
@@ -13,15 +14,61 @@ describe('Loader', () => {
     autoExampleDriver.reset();
   });
 
-  it('should render given valid props', () => {
+  eyes.it('should render given valid props', async () => {
     autoExampleDriver.setProps({size: 'large', color: 'white', text: 'Wubba Lubba Dub Dub'});
 
-    waitForVisibilityOf(loaderDriver.element(), 'Cannot find <Loader/>')
-      .then(() => {
-        expect(loaderDriver.isLarge()).toBe(true);
-        expect(loaderDriver.getColor()).toBe('white');
-        expect(loaderDriver.hasText()).toBe(true);
-        expect(loaderDriver.getText()).toBe('WUBBA LUBBA DUB DUB');
-      });
+    await waitForVisibilityOf(loaderDriver.element(), 'Cannot find <Loader/>');
+    expect(loaderDriver.isLarge()).toBe(true);
+    expect(loaderDriver.getColor()).toBe('white');
+    expect(loaderDriver.hasText()).toBe(true);
+    expect(loaderDriver.getText()).toBe('WUBBA LUBBA DUB DUB');
+  });
+
+  eyes.it('should render different Loader sizes', async () => {
+    autoExampleDriver.setProps({size: 'tiny'});
+    await waitForVisibilityOf(loaderDriver.element(), 'Cannot find <Loader/>');
+
+    eyes.checkWindow('tiny loader');
+
+    expect(loaderDriver.isTiny()).toBe(true);
+
+    autoExampleDriver.setProps({size: 'small'});
+    await waitForVisibilityOf(loaderDriver.element(), 'Cannot find <Loader/>');
+
+    eyes.checkWindow('small loader');
+
+    expect(loaderDriver.isSmall()).toBe(true);
+
+    autoExampleDriver.setProps({size: 'medium'});
+    await waitForVisibilityOf(loaderDriver.element(), 'Cannot find <Loader/>');
+
+    eyes.checkWindow('medium loader');
+
+    expect(loaderDriver.isMedium()).toBe(true);
+
+    autoExampleDriver.setProps({size: 'large'});
+    await waitForVisibilityOf(loaderDriver.element(), 'Cannot find <Loader/>');
+
+    eyes.checkWindow('large loader');
+
+    expect(loaderDriver.isLarge()).toBe(true);
+  });
+
+  eyes.it('should render different loader colors', async () => {
+    autoExampleDriver.setProps({color: 'blue'});
+
+    await waitForVisibilityOf(loaderDriver.element(), 'Cannot find <Loader/>');
+
+    eyes.checkWindow('blue loader');
+
+    expect(loaderDriver.getColor()).toBe('blue');
+
+    autoExampleDriver.setProps({color: 'white'});
+
+    await waitForVisibilityOf(loaderDriver.element(), 'Cannot find <Loader/>');
+
+    eyes.checkWindow('white loader');
+
+    expect(loaderDriver.getColor()).toBe('white');
   });
 });
