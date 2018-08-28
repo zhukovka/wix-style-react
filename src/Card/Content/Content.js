7b01f45aab4afbc7ee99fb1350d410db3c2be4f3
Arijus Šukys <argshook@gmail.com>
2018-08-28 14:24:13 +0300

    chore(Card.EmptyState): remove additional component and simply use composition (#2130)
    * chore(Card.EmptyState): remove additional component and simply use composition
    
    * test(Card.Content): test for EmptyState child
    
    * chore(Card.EmptyState): remove unused code

--- src/Card/Content/Content.js
+++ src/Card/Content/Content.js
@@ -3,15 +3,19 @@ import {node} from 'prop-types';
 import styles from './Content.scss';
 
 class Content extends Component {
-
   static propTypes = {
     children: node
   };
 
+  _getChildName = children =>
+    children.type && (children.type.name || children.type.displayName);
+
   render() {
+    const {children} = this.props;
+
     return (
-      <div className={styles.content}>
-        {this.props.children}
+      <div className={this._getChildName(children) === 'EmptyState' ? styles.emptyStateContent : styles.content}>
+        {children}
       </div>
     );
   }
