58ab99f4251754b2cd13b5e0af62490778651d53
Erez Makavy <erezm@wix.com>
2018-09-13 18:20:58 +0300

    Remove dead code `src/Backoffice./ButtonLayout` (#2231)
    * Remove old Backoffice/ButtonLayout
    
    * Fix DatePicker flaky e2e test
    
    * Fix Flaky Text e2e test
    
    * Text e2e - remove example when not needed
    
    * Text e2e - fix before->beforeAll

--- src/DatePicker/DatePicker.e2e.js
+++ src/DatePicker/DatePicker.e2e.js
@@ -7,127 +7,127 @@ describe('DatePicker', () => {
   const storyUrl = getStoryUrl('3. Inputs', '3.6 DatePicker');
   const {inputDriver, calendarDriver} = datePickerTestkitFactory({dataHook: 'storybook-datepicker'});
 
-  beforeAll(() => {
-    browser.get(storyUrl);
+  beforeAll(async () => {
+    await browser.get(storyUrl);
   });
 
-  beforeEach(() => {
-    autoExampleDriver.reset();
+  beforeEach(async () => {
+    await autoExampleDriver.reset();
   });
 
-  eyes.it('should not open calendar when disabled', () => {
-    autoExampleDriver.setProps({disabled: true});
-    inputDriver.click();
+  eyes.it('should not open calendar when disabled', async () => {
+    await autoExampleDriver.setProps({disabled: true});
+    await inputDriver.click();
 
-    expect(calendarDriver.exists()).toBe(false);
+    expect(await calendarDriver.exists()).toBe(false);
   });
 
-  eyes.it('should not close calendar on selecting date with click when shouldCloseOnSelect prop set to false', () => {
-    autoExampleDriver.setProps({shouldCloseOnSelect: false});
-    inputDriver.click();
+  eyes.it('should not close calendar on selecting date with click when shouldCloseOnSelect prop set to false', async () => {
+    await autoExampleDriver.setProps({shouldCloseOnSelect: false});
+    await inputDriver.click();
 
-    calendarDriver.clickOnNthAvailableDay(10);
+    await calendarDriver.clickOnNthAvailableDay(10);
 
-    expect(calendarDriver.exists()).toBe(true);
-    expect(calendarDriver.isVisible()).toBe(true);
+    expect(await calendarDriver.exists()).toBe(true);
+    expect(await calendarDriver.isVisible()).toBe(true);
   });
 
   describe('default', () => {
-    eyes.it('should show inputDriver', () => {
-      expect(inputDriver.isVisible()).toBe(true);
+    eyes.it('should show inputDriver', async () => {
+      expect(await inputDriver.isVisible()).toBe(true);
     });
 
-    eyes.it('should open calendar when click on inputDriver', () => {
-      inputDriver.click();
-      expect(calendarDriver.exists()).toBe(true);
-      expect(calendarDriver.isVisible()).toBe(true);
+    eyes.it('should open calendar when click on inputDriver', async () => {
+      await inputDriver.click();
+      expect(await calendarDriver.exists()).toBe(true);
+      expect(await calendarDriver.isVisible()).toBe(true);
     }, {version: '<Input/> - On text click - select all'});
 
-    eyes.it('should close calendar on selecting date with click', () => {
-      inputDriver.click();
-      calendarDriver.clickOnNthAvailableDay(1);
-      expect(calendarDriver.exists()).toBe(false);
+    eyes.it('should close calendar on selecting date with click', async () => {
+      await inputDriver.click();
+      await calendarDriver.clickOnNthAvailableDay(1);
+      expect(await calendarDriver.exists()).toBe(false);
     });
 
-    eyes.it('should close calendar on Escape key', () => {
-      inputDriver.click();
-      expect(calendarDriver.exists()).toBe(true);
+    eyes.it('should close calendar on Escape key', async () => {
+      await inputDriver.click();
+      expect(await calendarDriver.exists()).toBe(true);
 
-      calendarDriver.pressEscKey();
-      expect(calendarDriver.exists()).toBe(false);
+      await calendarDriver.pressEscKey();
+      expect(await calendarDriver.exists()).toBe(false);
     }, {version: '<Input/> - On text click - select all'});
 
-    eyes.it('should close calendar on Tab key', () => {
-      inputDriver.click();
-      expect(calendarDriver.exists()).toBe(true);
+    eyes.it('should close calendar on Tab key', async () => {
+      await inputDriver.click();
+      expect(await calendarDriver.exists()).toBe(true);
 
-      calendarDriver.pressTabKey();
-      expect(calendarDriver.exists()).toBe(false);
+      await calendarDriver.pressTabKey();
+      expect(await calendarDriver.exists()).toBe(false);
     });
 
-    eyes.it('should not change date', () => {
-      autoExampleDriver.setProps({value: new Date('2017/05/01')});
-      inputDriver.click();
-      calendarDriver.pressEnterKey();
+    eyes.it('should not change date', async () => {
+      await autoExampleDriver.setProps({value: new Date('2017/05/01')});
+      await inputDriver.click();
+      await calendarDriver.pressEnterKey();
 
-      expect(inputDriver.getValue()).toBe('2017/05/01');
+      expect(await inputDriver.getValue()).toBe('2017/05/01');
     }, {version: '<Input/> - On text click - select all'});
 
-    eyes.it('should select next day date', () => {
-      autoExampleDriver.setProps({value: new Date('2017/05/01')});
-      inputDriver.click();
-      calendarDriver.pressArrowRightKey();
-      calendarDriver.pressEnterKey();
+    eyes.it('should select next day date', async () => {
+      await autoExampleDriver.setProps({value: new Date('2017/05/01')});
+      await inputDriver.click();
+      await calendarDriver.pressArrowRightKey();
+      await calendarDriver.pressEnterKey();
 
-      expect(inputDriver.getValue()).toBe('2017/05/02');
+      expect(await inputDriver.getValue()).toBe('2017/05/02');
     });
   });
 
   describe('with year and month dropdown', () => {
-    beforeEach(() =>
-      autoExampleDriver.setProps({
+    beforeEach(async () =>
+      await autoExampleDriver.setProps({
         showYearDropdown: true,
         showMonthDropdown: true,
         value: new Date('2017/05/01')
       })
     );
 
-    eyes.it('should be scrolled to current year', () => {
-      inputDriver.click();
-      calendarDriver.openYearDropdownOptions();
-      expect(calendarDriver.isYearInViewPort('2017')).toBeTruthy();
+    eyes.it('should be scrolled to current year', async () => {
+      await inputDriver.click();
+      await calendarDriver.openYearDropdownOptions();
+      expect(await calendarDriver.isYearInViewPort('2017')).toBeTruthy();
     });
 
-    eyes.it('should select 2027 year', () => {
-      inputDriver.click();
+    eyes.it('should select 2027 year', async () => {
+      await inputDriver.click();
 
-      calendarDriver.openYearDropdownOptions();
-      calendarDriver.clickOnNthYear();
-      calendarDriver.clickOnNthAvailableDay();
+      await calendarDriver.openYearDropdownOptions();
+      await calendarDriver.clickOnNthYear();
+      await calendarDriver.clickOnNthAvailableDay();
 
-      expect(inputDriver.getValue()).toBe('2027/05/01');
+      expect(await inputDriver.getValue()).toBe('2027/05/01');
     });
 
-    eyes.it('should select February', () => {
-      inputDriver.click();
+    eyes.it('should select February', async () => {
+      await inputDriver.click();
 
-      calendarDriver.openMonthDropdownOptions();
-      calendarDriver.clickOnNthMonth(2);
-      calendarDriver.clickOnNthAvailableDay();
+      await calendarDriver.openMonthDropdownOptions();
+      await calendarDriver.clickOnNthMonth(2);
+      await calendarDriver.clickOnNthAvailableDay();
 
-      expect(inputDriver.getValue()).toBe('2017/02/01');
+      expect(await inputDriver.getValue()).toBe('2017/02/01');
     });
 
-    eyes.it('should select February and 2026 year', () => {
-      inputDriver.click();
+    eyes.it('should select February and 2026 year', async () => {
+      await inputDriver.click();
 
-      calendarDriver.openYearDropdownOptions();
-      calendarDriver.clickOnNthYear(2);
-      calendarDriver.openMonthDropdownOptions();
-      calendarDriver.clickOnNthMonth(2);
-      calendarDriver.clickOnNthAvailableDay();
+      await calendarDriver.openYearDropdownOptions();
+      await calendarDriver.clickOnNthYear(2);
+      await calendarDriver.openMonthDropdownOptions();
+      await calendarDriver.clickOnNthMonth(2);
+      await calendarDriver.clickOnNthAvailableDay();
 
-      expect(inputDriver.getValue()).toBe('2026/02/01');
+      expect(await inputDriver.getValue()).toBe('2026/02/01');
     });
   });
 });
--- src/Text/Text.e2e.js
+++ src/Text/Text.e2e.js
@@ -1,90 +1,84 @@
 import eyes from 'eyes.it';
 import autoExampleDriver from 'wix-storybook-utils/AutoExampleDriver';
-import {getStoryUrl, waitForVisibilityOf} from 'wix-ui-test-utils/protractor';
+import {createStoryUrl, waitForVisibilityOf} from 'wix-ui-test-utils/protractor';
 import {tooltipTestkitFactory} from 'wix-ui-core/dist/src/testkit/protractor';
 import {textTestkitFactory} from '../../testkit/protractor';
 import {SIZES, SKINS, WEIGHTS} from './Text';
 
 describe('Text', () => {
-  const storyUrl = getStoryUrl('1. Foundation', '1.2 Text');
+  const storyUrl = createStoryUrl({kind: '1. Foundation', story: '1.2 Text', withExamples: false});
+  const storyUrlWithExamples = createStoryUrl({kind: '1. Foundation', story: '1.2 Text', withExamples: true});
 
-  beforeEach(() => browser.get(storyUrl));
-  afterEach(() => autoExampleDriver.reset());
-
-  eyes.it('should display correct content', () => {
-    const dataHook = 'storybook-text';
+  const init = async ({url, dataHook}) => {
+    await browser.get(url);
     const driver = textTestkitFactory({dataHook});
-
-    return waitForVisibilityOf(driver.element(), 'Cannot find Text')
-      .then(() => expect(driver.getText()).toBe('Some text'));
-  });
-
-  eyes.it('light prop', async () => {
-    const dataHook = 'storybook-text';
-    const driver = textTestkitFactory({dataHook});
-
     await waitForVisibilityOf(driver.element(), 'Cannot find Text');
-    await eyes.checkWindow('dark');
+    return driver;
+  };
 
-    await autoExampleDriver.setProps({light: true});
-    await waitForVisibilityOf(driver.element(), 'Cannot find Text');
-    await eyes.checkWindow('light');
-  });
-
-  eyes.it('size prop', async () => {
-    const dataHook = 'storybook-text';
-    const driver = textTestkitFactory({dataHook});
-
-    Object.keys(SIZES).forEach(async size => {
-      await autoExampleDriver.setProps({size});
-      await waitForVisibilityOf(driver.element(), 'Cannot find Text');
-      await eyes.checkWindow(size);
+  describe('AutoExample', () => {
+    let driver;
+    beforeAll(async () => {
+      driver = await init({url: storyUrl, dataHook: 'storybook-text'});
     });
-  });
+    afterEach(() => autoExampleDriver.reset());
 
-  eyes.it('skin prop', async () => {
-    const dataHook = 'storybook-text';
-    const driver = textTestkitFactory({dataHook});
-
-    Object.keys(SKINS).forEach(async skin => {
-      await autoExampleDriver.setProps({skin});
-      await waitForVisibilityOf(driver.element(), 'Cannot find Text');
-      await eyes.checkWindow(skin);
+    eyes.it('should display correct content', async () => {
+      expect(await driver.getText()).toBe('Some text');
     });
-  });
 
-  describe('weight prop', () => {
-    eyes.it('should display "standard" weight', async () => {
-      const dataHook = 'storybook-text';
-      const driver = textTestkitFactory({dataHook});
+    eyes.it('light prop', async () => {
+      await eyes.checkWindow('dark');
 
-      Object.keys(WEIGHTS).forEach(async weight => {
+      await autoExampleDriver.setProps({light: true});
+      await waitForVisibilityOf(driver.element(), 'Cannot find Text');
+      await eyes.checkWindow('light');
+    });
+
+    eyes.it('size prop', async () => {
+      for (const size of Object.keys(SIZES)) {
+        await autoExampleDriver.setProps({size});
+        await waitForVisibilityOf(driver.element(), 'Cannot find Text');
+        await eyes.checkWindow(size);
+      }
+    });
+
+    eyes.it('skin prop', async () => {
+      for (const skin of Object.keys(SKINS)) {
+        await autoExampleDriver.setProps({skin});
+        await waitForVisibilityOf(driver.element(), 'Cannot find Text');
+        await eyes.checkWindow(skin);
+      }
+    });
+
+    eyes.it('weight prop', async () => {
+      for (const weight of Object.keys(WEIGHTS)) {
         await autoExampleDriver.setProps({weight});
         await waitForVisibilityOf(driver.element(), 'Cannot find Text');
         await eyes.checkWindow(weight);
-      });
+      }
     });
   });
 
   describe('with tooltip', () => {
     eyes.it('should not show tooltip on hover when text is not truncated with ellipses', async () => {
       const dataHook = 'storybook-text';
-      const driver = textTestkitFactory({dataHook});
+      const driver = await init({url: storyUrl, dataHook});
       const tooltipDriver = tooltipTestkitFactory({dataHook});
       await waitForVisibilityOf(driver.element(), 'Cannot find Text');
-      expect(tooltipDriver.isContentElementExists()).toBeFalsy();
+      expect(await tooltipDriver.isContentElementExists()).toBeFalsy();
       await tooltipDriver.mouseEnter();
-      expect(tooltipDriver.isContentElementExists()).toBeFalsy();
+      expect(await tooltipDriver.isContentElementExists()).toBeFalsy();
     });
 
     eyes.it('should show tooltip on hover when text is truncated with ellipses', async () => {
       const dataHook = 'text-with-ellipses';
-      const driver = textTestkitFactory({dataHook});
+      const driver = await init({url: storyUrlWithExamples, dataHook});
       const tooltipDriver = tooltipTestkitFactory({dataHook});
       await waitForVisibilityOf(driver.element(), 'Cannot find Text');
-      expect(tooltipDriver.isContentElementExists()).toBeFalsy();
+      expect(await tooltipDriver.isContentElementExists()).toBeFalsy();
       await tooltipDriver.mouseEnter();
-      expect(tooltipDriver.isContentElementExists()).toBeTruthy();
+      expect(await tooltipDriver.isContentElementExists()).toBeTruthy();
     });
   });
 });
