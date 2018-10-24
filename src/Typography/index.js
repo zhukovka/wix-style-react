d7f951e767a3b1f6d7568ab061cbf65bd63f04b1
Erez Makavy <erezm@wix.com>
2018-10-24 15:18:43 +0300

    Hot-Fix! Typography deprecationLog - no proxy in production (#2370)

--- src/Typography/index.js
+++ src/Typography/index.js
@@ -3,19 +3,24 @@ import deprecationLog from '../utils/deprecationLog';
 
 export * from './Utils';
 
-const deprecatedRegExp = new RegExp('^([ht][1-6]_[1-6]|t[1-6])$');
+let typographyProxy = typography;
 
-const typographyProxy = new Proxy(typography, {
-  get(target, prop) {
-    if (deprecatedRegExp.test(prop)) {
-      deprecationLog(
+// Proxy is not supported in IE11, so we enable it only for development
+if (process.env.NODE_ENV !== 'production') {
+  const deprecatedRegExp = new RegExp('^([ht][1-6]_[1-6]|t[1-6])$');
+
+  typographyProxy = new Proxy(typography, { // eslint-disable-line no-restricted-globals
+    get(target, prop) {
+      if (deprecatedRegExp.test(prop)) {
+        deprecationLog(
         `Typography class ${prop} is deprecated. Please use new classes described at https://wix-wix-style-react.surge.sh/?selectedKind=Styling&selectedStory=1.2%20Typography%20Classes`,
         prop
         );
+      }
+      return target[prop];
     }
-    return target[prop];
-  }
-});
+  });
+}
 
 export default typographyProxy;
 
