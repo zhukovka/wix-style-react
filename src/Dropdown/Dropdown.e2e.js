6f68a8840d25db265101343d816099e168cb1052
Yarden Sod-Moriah <yardnsm@gmail.com>
2018-10-29 17:18:16 +0200

    <DropdownLayout/> - fix scroll behaviour Pt.2 (#2365)
    ### What changed
    
    This is another fix for the scroll behaviour of `<DropdownLayout/>`. See https://github.com/wix/wix-style-react/pull/2327.
    
    This is more like commit https://github.com/wix/wix-style-react/pull/2327/commits/9f56aad52b4b8179889cc190c06d4d3b124c17c3 on steroids.
    
    ### Why it changed
    
    See:
    
    * https://github.com/wix/wix-style-react/issues/2169
    * https://github.com/wix/wix-style-react/pull/2327#issuecomment-431912097
    * https://github.com/wix/wix-style-react/pull/2362
    
    ---
    
    - [ ] Change is tested
    - [ ] Change is documented
    - [ ] Build is green
    - [ ] Change of UX is approved by [wuwa](https://github.com/wuwa) or [milkyfruit](https://github.com/milkyfruit)
    
    ### Thanks for contributing :)

--- src/Dropdown/Dropdown.e2e.js
+++ src/Dropdown/Dropdown.e2e.js
@@ -56,4 +56,81 @@ describe('Dropdown', () => {
     await driver.getInput().click();
     expect(await driver.isOptionsShown().isDisplayed()).toBeTruthy();
   });
+
+  describe('scrolling behaviour', () => {
+    const options = Array(30).fill().map((v, i) => ({id: i, value: `Option ${i}`}));
+    let driver;
+
+    const repeat = (times, fn) =>
+      Array(times)
+        .fill()
+        .reduce(promise => (
+          promise.then(() => fn())
+        ), Promise.resolve());
+
+    const pressArrowDown = (times = 0) => repeat(times, () => browser.actions().sendKeys(protractor.Key.ARROW_DOWN).perform());
+    const pressArrowUp = (times = 0) => repeat(times, () => browser.actions().sendKeys(protractor.Key.ARROW_UP).perform());
+
+    const getScrollTop = element => browser.executeScript('return arguments[0].scrollTop', element);
+    const getRectBottom = element => browser.executeScript('return arguments[0].getBoundingClientRect().bottom', element);
+    const getRectTop = element => browser.executeScript('return arguments[0].getBoundingClientRect().top', element);
+
+    beforeAll(async () => {
+      await browser.get(autoExampleUrl);
+    });
+
+    beforeEach(async () => {
+      driver = dropdownTestkitFactory({dataHook: 'story-dropdown'});
+      await waitForVisibilityOf(driver.element(), 'Cannot find Dropdown');
+      await autoExampleDriver.setProps({options});
+    });
+
+    afterEach(async () => {
+      await autoExampleDriver.remount();
+    });
+
+    /*
+     * In our example, the Drodpown can hold about 6 full items in the view.
+     */
+
+    eyes.it('should not change scroll when hovered option is in view', async () => {
+      await driver.getInput().click();
+
+      // Scrolling manually 3 items down. The 3rd item should be in view.
+      await pressArrowDown(3);
+
+      // `scrollTop` should not change
+      expect(await getScrollTop(driver.getDropdown())).toEqual(0);
+    });
+
+    eyes.it('should change scroll when hovered option is below the view', async () => {
+      await driver.getInput().click();
+
+      // Scrolling manually 8 items down, the 8th item should be *below* view
+      await pressArrowDown(8);
+
+      // Container and option should be aligned to the bottom
+      expect(
+        await getRectBottom(driver.getDropdown())
+      ).toEqual(
+        await getRectBottom(driver.getDropdownItemElement(7))
+      );
+    });
+
+    eyes.it('should change scroll when hovered option is above the view', async () => {
+      await driver.getInput().click();
+
+      // Scroling down 16 item down, then 8 items up. The 8t item (when we
+      // scrolling up) should be *above* the view
+      await pressArrowDown(16);
+      await pressArrowUp(8);
+
+      // Container and option should be aligned to the top
+      expect(
+        await getRectTop(driver.getDropdown())
+      ).toEqual(
+        await getRectTop(driver.getDropdownItemElement(7))
+      );
+    });
+  });
 });
