3c2e293fbabfacf6c967d9bedc14f0267a8cf81b
Mykolas Sindeikis <mykolass@wix.com>
2018-11-06 14:48:35 +0200

    Upgrade <MessageBoxMarketerialLayout/> typography (#2411)
    * Upgrade AnouncementBox typography
    
    * Deprecation logging
    
    * fixIamgePosition
    
    * Fix action buttons typography

--- stories/MessageBox/AnnouncementExamples/PremiumAction.js
+++ stories/MessageBox/AnnouncementExamples/PremiumAction.js
@@ -5,6 +5,7 @@ import {MessageBoxMarketerialLayout} from 'wix-style-react/MessageBox';
 export default () => (
   <MessageBoxMarketerialLayout
     title="Looking good! Your site is on Google"
+    fixImagePosition
     content="All of your pages are indexed and now come up as separate search results on Google. This is great for your visbility!"
     confirmText="Button"
     imageUrl="https://static.wixstatic.com/media/9ab0d1_8f1d1bd00e6c4bcd8764e1cae938f872~mv1.png"
