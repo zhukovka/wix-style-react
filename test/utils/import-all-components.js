0fb07c17ab56edfef2d4ec1202165ef612bbda99
Arijus Šukys <argshook@gmail.com>
2018-11-12 11:12:14 +0200

    test(*): smoke test for all components vanilla & enzyme testkits (#2446)
    * test(*): smoke test for all components vanilla & enzyme testkits
    
    * chore(testkit-smoke): add comments, rename setupProps to REQUIRED_PROPS
    
    * chore(testkit-smoke): move utils lower
    
    let's keep parts to be declared by human at the top of the file

---
+++ test/utils/import-all-components.js
@@ -0,0 +1,42 @@
+import fs from 'fs';
+import path from 'path';
+
+const NON_COMPONENT_FOLDER_NAMES = [
+  'utils',
+  'providers',
+  'new-icons',
+  'dnd-styles',
+  'assets',
+  'clients',
+  'mixins',
+  'common',
+  'FieldLabelAttributes',
+  'Backoffice',
+  'BaseComponents',
+  'Typography',
+  'TPA',
+  'Animations'
+];
+
+const matches = haystack => needle => haystack.some(h => needle === h);
+
+const defaultOrRoot = object => object.default || object;
+
+const Components = ({cwd, ignore = []}) => {
+  const componentNameInvalidators = [
+    path.extname,
+    matches(NON_COMPONENT_FOLDER_NAMES.concat(ignore))
+  ];
+
+  const components = fs
+    .readdirSync(cwd)
+    .filter(file => !componentNameInvalidators.some(v => v(file)));
+
+  return components.reduce((a, c) => {
+    const ref = require(path.resolve(cwd, c));
+    a[c] = defaultOrRoot(ref);
+    return a;
+  }, {});
+};
+
+export default Components;
