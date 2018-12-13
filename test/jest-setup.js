95221a35ccdbb5340cc1bb7837222bce9f41141a
Yarden Sod-Moriah <yardnsm@gmail.com>
2018-12-13 18:20:08 +0200

    Add a default config to eyesItInstance (#2568)
    Also added some unit tests :)

--- test/jest-setup.js
+++ test/jest-setup.js
@@ -2,3 +2,6 @@ import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-15';
 
 Enzyme.configure({ adapter: new Adapter() });
+
+// Prevent `eyes.it` to initialize itself
+process.env.EYES_API_KEY = '';
