815c9ba1148570a194e87158e1dfeb9e80dc2318
mykas <mykolass@wix.com>
2019-01-09 15:43:04 +0200

    Tests refactor

--- src/SelectableList/SelectableList.spec.js
+++ src/SelectableList/SelectableList.spec.js
@@ -8,67 +8,63 @@ import Checkbox from '../Checkbox';
 import { checkboxTestkitFactory } from '../../testkit';
 
 describe('SelectableList', () => {
+  const dataHook1 = 'clicked-checkbox1';
+  const dataHook2 = 'clicked-checkbox2';
+
   const createDriver = createUniDriverFactory(
     selectableListPrivateDriverFactory,
   );
 
+  const renderSelectablelist = (props = {}) => (
+    <SelectableList {...props}>
+      <Checkbox dataHook={dataHook1} />
+      <Checkbox dataHook={dataHook2} />
+      <Checkbox />
+    </SelectableList>
+  );
+
   it('should render multiple children', async () => {
-    const driver = createDriver(
-      <SelectableList>
-        <Checkbox />
-        <Checkbox />
-        <Checkbox />
-      </SelectableList>,
-    );
+    const driver = createDriver(renderSelectablelist());
     expect(await driver.exists()).toBeTruthy();
   });
 
   it('`onSelect` should return selected component', async () => {
-    const dataHook = 'clicked-checkbox';
-
     const mockOnSelect = jest.fn();
 
     const driver = createDriver(
-      <SelectableList onSelect={mockOnSelect} trigger="checked">
-        <Checkbox />
-        <Checkbox />
-        <Checkbox dataHook={dataHook} />
-      </SelectableList>,
+      renderSelectablelist({ onSelect: mockOnSelect }),
     );
 
     const element = await driver.element();
 
     const checkboxDriver = checkboxTestkitFactory({
       wrapper: element,
-      dataHook: dataHook,
+      dataHook: dataHook1,
     });
 
-    await checkboxDriver.click();
+    checkboxDriver.click();
 
     const mockCallback = mockOnSelect.mock.calls[0][1];
 
-    expect(mockCallback.props.dataHook).toBe(dataHook);
+    expect(mockCallback.props.dataHook).toBe(dataHook1);
   });
 
   it('`onDeselect` should return deselected component', async () => {
-    const dataHook = 'clicked-checkbox';
-
     const mockOnSelect = jest.fn();
     const mockOnDeselect = jest.fn();
 
     const driver = createDriver(
-      <SelectableList onSelect={mockOnSelect} onDeselect={mockOnDeselect}>
-        <Checkbox />
-        <Checkbox />
-        <Checkbox dataHook={dataHook} />
-      </SelectableList>,
+      renderSelectablelist({
+        onSelect: mockOnSelect,
+        onDeselect: mockOnDeselect,
+      }),
     );
 
     const element = await driver.element();
 
     const checkboxDriver = checkboxTestkitFactory({
       wrapper: element,
-      dataHook: dataHook,
+      dataHook: dataHook1,
     });
 
     checkboxDriver.click();
@@ -77,21 +73,17 @@ describe('SelectableList', () => {
     const mockonDeselectCallback = mockOnDeselect.mock.calls[0][1];
 
     expect(mockOnSelect).toHaveBeenCalledTimes(1);
-    expect(mockonDeselectCallback.props.dataHook).toBe(dataHook);
+    expect(mockonDeselectCallback.props.dataHook).toBe(dataHook1);
   });
 
   it('`limit` prop should limit amount of clicked items', async () => {
-    const dataHook1 = 'clicked-checkbox1';
-    const dataHook2 = 'clicked-checkbox2';
-
     const mockOnSelect = jest.fn();
 
     const driver = createDriver(
-      <SelectableList limit={1} onSelect={mockOnSelect}>
-        <Checkbox />
-        <Checkbox dataHook={dataHook1} />
-        <Checkbox dataHook={dataHook2} />
-      </SelectableList>,
+      renderSelectablelist({
+        onSelect: mockOnSelect,
+        limit: 1,
+      }),
     );
 
     const element = await driver.element();
@@ -116,24 +108,22 @@ describe('SelectableList', () => {
   });
 
   it('`trigger` prop should be used to trigger children state', async () => {
-    const dataHook = 'clicked-checkbox';
     const trigger = 'checked';
 
     const mockOnSelect = jest.fn();
 
     const driver = createDriver(
-      <SelectableList onSelect={mockOnSelect} trigger={trigger}>
-        <Checkbox />
-        <Checkbox />
-        <Checkbox dataHook={dataHook} />
-      </SelectableList>,
+      renderSelectablelist({
+        onSelect: mockOnSelect,
+        trigger,
+      }),
     );
 
     const element = await driver.element();
 
     const checkboxDriver = checkboxTestkitFactory({
       wrapper: element,
-      dataHook: dataHook,
+      dataHook: dataHook1,
     });
 
     checkboxDriver.click();
