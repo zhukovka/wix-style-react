b6c46fa33e6e3d852c59a22e7faafa189a5cfcfd
Nitay Rabinovich <nitayr@wix.com>
2019-01-31 17:05:45 +0200

    [WIP] - Integrate Google analytics with Storybook (#2860)
    * Initial google analytics implementation
    
    * Fix link to addon, move var to config
    
    * Use 4.0.0 fo google analytics
    
    * Remove redundant file

--- .storybook/config.js
+++ .storybook/config.js
@@ -5,6 +5,9 @@ import { version } from "../package.json";
 
 import "../src/assets/helvetica.scss";
 
+// Set google analytics ID Var;
+window.STORYBOOK_GA_ID = 'UA-133585953-1';
+
 function loadStories() {
   if (global.self === global.top) {
     require("./e2e-styles.scss");
