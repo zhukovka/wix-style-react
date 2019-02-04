91416b686afd789c5e6516f3e05f1e2f6f8869ec
Mykolas Sindeikis <mykolass@wix.com>
2019-02-04 15:38:33 +0200

    <Button/> - improve and tweak documentation (#2880)
    * <Button/> - improve and tweak documentation
    
    * <Button/> - icrease button examples sizing
    
    * <Button/> - reoredering example item
    
    * <Button/> - tweak react router examples
    
    * <Button/> - make sure that livecode examples uses baseScope
    
    * <Buton/> - refactor liveCode module
    
    * <Button/> - remove upgrade props from other components
    
    * <Button/> - docs. Use manually written testkit docs.
    
    * <Button/> - testkit docs. Promise based arguents
    
    * <Button/> - add as prop in proptypes

--- stories/Button/examples/ExamplesSuffix.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable */
-<div className={styles.spacing}>
-  <Button upgrade size="tiny" suffixIcon={<Add />}>tiny</Button>
-  <Button upgrade size="small" suffixIcon={<Add />}>small</Button>
-  <Button upgrade size="medium" suffixIcon={<Add />}>medium</Button>
-  <Button upgrade size="large" suffixIcon={<Add />}>large</Button>
-</div>
