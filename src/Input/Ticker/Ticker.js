17bfe876f0d5b35dc852268ca28d515790d18a6b
Nitay Rabinovich <nitayr@wix.com>
2019-02-20 15:44:00 +0200

    <Input > - fix disabled ticker color (#2986)

--- src/Input/Ticker/Ticker.js
+++ src/Input/Ticker/Ticker.js
@@ -8,19 +8,21 @@ import InputConsumer from '../InputConsumer';
 
 const Ticker = ({ onUp, onDown, upDisabled, downDisabled, dataHook }) => (
   <InputConsumer consumerCompName={Ticker.displayName}>
-    {props => (
+    {({ disabled }) => (
       <div className={styles.root} data-hook={dataHook}>
         <div
-          className={classnames(styles.up, { [styles.disabled]: upDisabled })}
-          onClick={upDisabled ? null : onUp}
+          className={classnames(styles.up, {
+            [styles.disabled]: upDisabled || disabled,
+          })}
+          onClick={upDisabled || disabled ? null : onUp}
         >
           <FormFieldSpinnerUp />
         </div>
         <div
           className={classnames(styles.down, {
-            [styles.disabled]: downDisabled,
+            [styles.disabled]: downDisabled || disabled,
           })}
-          onClick={downDisabled ? null : onDown}
+          onClick={downDisabled || disabled ? null : onDown}
         >
           <FormFieldSpinnerDown />
         </div>
