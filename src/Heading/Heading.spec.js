a557958e750f813a43a0e6ace21c16bb50705b0f
Nitay Rabinovich <nitayr@wix.com>
2019-02-20 11:25:29 +0200

    <Heading> - Remove enzyme testkit

--- src/Heading/Heading.spec.js
+++ src/Heading/Heading.spec.js
@@ -2,11 +2,6 @@ import React from 'react';
 import headingDriverFactory from './Heading.driver';
 import Heading from './Heading';
 import { createDriverFactory } from 'wix-ui-test-utils/driver-factory';
-import { isEnzymeTestkitExists } from 'wix-ui-test-utils/enzyme';
-import { isTestkitExists } from 'wix-ui-test-utils/vanilla';
-import { mount } from 'enzyme';
-import { headingTestkitFactory } from '../../testkit';
-import { headingTestkitFactory as enzymeHeadingTestkitFactory } from '../../testkit/enzyme';
 
 describe('Heading', () => {
   const createDriver = createDriverFactory(headingDriverFactory);
@@ -20,26 +15,6 @@ describe('Heading', () => {
     it('should be light', () => {
       const wrapper = createDriver(<Heading light>Hello</Heading>);
       expect(wrapper.isLight()).toBe(true);
-    });
-  });
-
-  describe('testkit', () => {
-    it('should exist', () => {
-      expect(
-        isTestkitExists(<Heading>Hello World</Heading>, headingTestkitFactory),
-      ).toBe(true);
-    });
-  });
-
-  describe('enzyme testkit', () => {
-    it('should exist', () => {
-      expect(
-        isEnzymeTestkitExists(
-          <Heading>Hello World</Heading>,
-          enzymeHeadingTestkitFactory,
-          mount,
-        ),
-      ).toBe(true);
     });
   });
 });
