0d2fa0de744eebf573dc9620d363be9d4b55aaf3
Nitay Rabinovich <nitayr@wix.com>
2019-03-06 16:00:01 +0200

    Add puppeteer driver

--- testkit/puppeteer.js
+++ testkit/puppeteer.js
@@ -6,174 +6,155 @@ import {
 
 import inputDriverFactory from '../src/Input/Input.puppeteer.driver';
 
-export const inputTestkitFactory = puppeteerTestkitFactoryCreator (
-  inputDriverFactory
+export const inputTestkitFactory = puppeteerTestkitFactoryCreator(
+  inputDriverFactory,
 );
 
-import formFieldDriverFactory
-  from '../src/FormField/FormField.puppeteer.driver';
+import formFieldDriverFactory from '../src/FormField/FormField.puppeteer.driver';
 
-export const formFieldTestkitFactory = puppeteerTestkitFactoryCreator (
-  formFieldDriverFactory
+export const formFieldTestkitFactory = puppeteerTestkitFactoryCreator(
+  formFieldDriverFactory,
 );
 
 import tableDriverFactory from '../src/Table/Table.puppeteer.driver';
 
-export const tableTestkitFactory = puppeteerTestkitFactoryCreator (
-  tableDriverFactory
+export const tableTestkitFactory = puppeteerTestkitFactoryCreator(
+  tableDriverFactory,
 );
 
 import headingDriverFactory from '../src/Heading/Heading.puppeteer.driver';
 
-export const headingTestkitFactory = puppeteerTestkitFactoryCreator (
-  headingDriverFactory
+export const headingTestkitFactory = puppeteerTestkitFactoryCreator(
+  headingDriverFactory,
 );
 
 import textDriverFactory from '../src/Text/Text.puppeteer.driver';
 
-export const textTestkitFactory = puppeteerTestkitFactoryCreator (
-  textDriverFactory
+export const textTestkitFactory = puppeteerTestkitFactoryCreator(
+  textDriverFactory,
 );
 
 import tooltipDriverFactory from '../src/Tooltip/Tooltip.puppeteer.driver';
 
-export const tooltipTestkitFactory = puppeteerTestkitFactoryCreator (
-  tooltipDriverFactory
+export const tooltipTestkitFactory = puppeteerTestkitFactoryCreator(
+  tooltipDriverFactory,
 );
 
 // wix-ui-core (unidriver)
 
-import {textButtonDriverFactory} from '../src/TextButton/TextButton.driver';
+import { textButtonDriverFactory } from '../src/TextButton/TextButton.driver';
 
-export const textButtonTestkitFactory = puppeteerUniTestkitFactoryCreator (
-  textButtonDriverFactory
+export const textButtonTestkitFactory = puppeteerUniTestkitFactoryCreator(
+  textButtonDriverFactory,
 );
 
-import {avatarDriverFactory} from '../src/Avatar/Avatar.driver';
+import { avatarDriverFactory } from '../src/Avatar/Avatar.driver';
 
-export const avatarTestkitFactory = puppeteerUniTestkitFactoryCreator (
-  avatarDriverFactory
+export const avatarTestkitFactory = puppeteerUniTestkitFactoryCreator(
+  avatarDriverFactory,
 );
 
-import {iconButtonDriverFactory} from '../src/IconButton/IconButton.driver';
+import { iconButtonDriverFactory } from '../src/IconButton/IconButton.driver';
 
-export const iconButtonTestkitFactory = puppeteerUniTestkitFactoryCreator (
-  iconButtonDriverFactory
+export const iconButtonTestkitFactory = puppeteerUniTestkitFactoryCreator(
+  iconButtonDriverFactory,
 );
 
-import {closeButtonDriverFactory} from '../src/CloseButton/CloseButton.driver';
+import { closeButtonDriverFactory } from '../src/CloseButton/CloseButton.driver';
 
-export const closeButtonTestkitFactory = puppeteerUniTestkitFactoryCreator (
-  closeButtonDriverFactory
+export const closeButtonTestkitFactory = puppeteerUniTestkitFactoryCreator(
+  closeButtonDriverFactory,
 );
 
-import {proportionDriverFactory} from '../src/Proportion/Proportion.driver';
+import { proportionDriverFactory } from '../src/Proportion/Proportion.driver';
 
-export const proportionTestkitFactory = puppeteerUniTestkitFactoryCreator (
-  proportionDriverFactory
+export const proportionTestkitFactory = puppeteerUniTestkitFactoryCreator(
+  proportionDriverFactory,
 );
 
 /*
  * Component generator test component
  */
-import {
+import { generatedTestComponentDriverFactory } from '../src/GeneratedTestComponent/GeneratedTestComponent.uni.driver';
+
+export const generatedTestComponentTestkitFactory = puppeteerUniTestkitFactoryCreator(
   generatedTestComponentDriverFactory,
-} from '../src/GeneratedTestComponent/GeneratedTestComponent.uni.driver';
-
-export const generatedTestComponentTestkitFactory = puppeteerUniTestkitFactoryCreator (
-  generatedTestComponentDriverFactory
 );
 
-import {
+import { dropdownBaseDriverFactory } from '../src/DropdownBase/DropdownBase.driver';
+
+export const dropdownBaseTestkitFactory = puppeteerUniTestkitFactoryCreator(
   dropdownBaseDriverFactory,
-} from '../src/DropdownBase/DropdownBase.driver';
-
-export const dropdownBaseTestkitFactory = puppeteerUniTestkitFactoryCreator (
-  dropdownBaseDriverFactory
 );
 
-import {
+import { calendarPanelFooterDriverFactory } from '../src/CalendarPanelFooter/CalendarPanelFooter.driver';
+
+export const calendarPanelFooterTestkitFactory = puppeteerUniTestkitFactoryCreator(
   calendarPanelFooterDriverFactory,
-} from '../src/CalendarPanelFooter/CalendarPanelFooter.driver';
-
-export const calendarPanelFooterTestkitFactory = puppeteerUniTestkitFactoryCreator (
-  calendarPanelFooterDriverFactory
 );
 
-import {boxDriverFactory} from '../src/Box/Box.driver';
+import { boxDriverFactory } from '../src/Box/Box.driver';
 
-export const boxTestkitFactory = puppeteerUniTestkitFactoryCreator (
-  boxDriverFactory
+export const boxTestkitFactory = puppeteerUniTestkitFactoryCreator(
+  boxDriverFactory,
 );
 
-import {buttonDriverFactory} from '../src/Button/Button.driver';
+import { buttonDriverFactory } from '../src/Button/Button.driver';
 
-export const buttonTestkitFactory = puppeteerUniTestkitFactoryCreator (
-  buttonDriverFactory
+export const buttonTestkitFactory = puppeteerUniTestkitFactoryCreator(
+  buttonDriverFactory,
 );
 
-import {thumbnailDriverFactory} from '../src/Thumbnail/Thumbnail.driver';
+import { thumbnailDriverFactory } from '../src/Thumbnail/Thumbnail.driver';
 
-export const thumbnailTestkitFactory = puppeteerUniTestkitFactoryCreator (
-  thumbnailDriverFactory
+export const thumbnailTestkitFactory = puppeteerUniTestkitFactoryCreator(
+  thumbnailDriverFactory,
 );
 
-import {
+import { segmentedToggleDriverFactory } from '../src/SegmentedToggle/SegmentedToggle.driver';
+
+export const segmentedToggleTestkitFactory = puppeteerUniTestkitFactoryCreator(
   segmentedToggleDriverFactory,
-} from '../src/SegmentedToggle/SegmentedToggle.driver';
-
-export const segmentedToggleTestkitFactory = puppeteerUniTestkitFactoryCreator (
-  segmentedToggleDriverFactory
 );
 
-import {
+import { richTextInputAreaDriverFactory } from '../src/RichTextInputArea/RichTextInputArea.driver';
+
+export const richTextInputAreaTestkitFactory = puppeteerUniTestkitFactoryCreator(
   richTextInputAreaDriverFactory,
-} from '../src/RichTextInputArea/RichTextInputArea.driver';
-
-export const richTextInputAreaTestkitFactory = puppeteerUniTestkitFactoryCreator (
-  richTextInputAreaDriverFactory
 );
 
-import {
+import { richTextToolbarDriverFactory } from '../src/RichTextInputArea/RichTextToolbar.driver';
+
+export const richTextToolbarTestkitFactory = puppeteerUniTestkitFactoryCreator(
   richTextToolbarDriverFactory,
-} from '../src/RichTextInputArea/RichTextToolbar.driver';
-
-export const richTextToolbarTestkitFactory = puppeteerUniTestkitFactoryCreator (
-  richTextToolbarDriverFactory
 );
 
-import {
+import { floatingNotificationDriverFactory } from '../src/FloatingNotification/FloatingNotification.driver';
+
+export const floatingNotificationTestkitFactory = puppeteerUniTestkitFactoryCreator(
   floatingNotificationDriverFactory,
-} from '../src/FloatingNotification/FloatingNotification.driver';
-
-export const floatingNotificationTestkitFactory = puppeteerUniTestkitFactoryCreator (
-  floatingNotificationDriverFactory
 );
 
-import {
+import { noBorderInputDriverFactory } from '../src/NoBorderInput/NoBorderInput.puppeteer.driver';
+
+export const noBorderInputTestkitFactory = puppeteerTestkitFactoryCreator(
   noBorderInputDriverFactory,
-} from '../src/NoBorderInput/NoBorderInput.puppeteer.driver';
-
-export const noBorderInputTestkitFactory = puppeteerTestkitFactoryCreator (
-  noBorderInputDriverFactory
 );
 
-import {colorInputDriverFactory} from '../src/ColorInput/ColorInput.driver';
+import { colorInputDriverFactory } from '../src/ColorInput/ColorInput.driver';
 
-export const colorInputTestkitFactory = puppeteerUniTestkitFactoryCreator (
-  colorInputDriverFactory
+export const colorInputTestkitFactory = puppeteerUniTestkitFactoryCreator(
+  colorInputDriverFactory,
 );
 
-import {dateInputDriverFactory} from '../src/DateInput/DateInput.uni.driver';
+import { dateInputDriverFactory } from '../src/DateInput/DateInput.uni.driver';
 
-export const dateInputTestkitFactory = puppeteerUniTestkitFactoryCreator (
-  dateInputDriverFactory
+export const dateInputTestkitFactory = puppeteerUniTestkitFactoryCreator(
+  dateInputDriverFactory,
 );
 
-import {
+import { dateRangeInputDriverFactory } from '../src/DateRangeInput/DateRangeInput.uni.driver';
+
+export const dateRangeInputTestkitFactory = puppeteerUniTestkitFactoryCreator(
   dateRangeInputDriverFactory,
-} from '../src/DateRangeInput/DateRangeInput.uni.driver';
-
-export const dateRangeInputTestkitFactory = puppeteerUniTestkitFactoryCreator (
-  dateRangeInputDriverFactory
 );
