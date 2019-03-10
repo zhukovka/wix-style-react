cdb193e72dfb8a34c851258d53f607b318e12d74
Nitay Rabinovich <nitayr@wix.com>
2019-03-10 13:53:49 +0200

    Remove redundant `kind` from storySettings

--- src/DateInput/docs/storySettings.js
+++ src/DateInput/docs/storySettings.js
@@ -1,7 +1,6 @@
-import { Category } from '../../../stories/storiesHierarchy';
+import {Category} from '../../../stories/storiesHierarchy';
 
 export const storySettings = {
-  kind: Category.WIP,
   category: Category.WIP,
   storyName: 'DateInput',
   dataHook: 'story-date-input',
