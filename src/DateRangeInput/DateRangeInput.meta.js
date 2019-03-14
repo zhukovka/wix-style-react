2935e7f7b940e09943afc454beb49c32cd488ef2
Nitay Rabinovich <nitayr@wix.com>
2019-03-14 17:32:15 +0200

    Remove redundant meta file

--- src/DateRangeInput/DateRangeInput.meta.js
+++
@@ -1,8 +0,0 @@
-import DateRangeInput from './DateRangeInput';
-import Registry from '@ui-autotools/registry';
-
-const metadata = Registry.getComponentMetadata(DateRangeInput);
-
-metadata.exportedFrom({
-  path: 'src/DateRangeInput/DateRangeInput.js',
-});
