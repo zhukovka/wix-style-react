b4582395beea7276039073a7577bb61b90062607
Arijus Šukys <argshook@gmail.com>
2019-03-15 12:28:52 +0200

    component generator: update generated story.js with sections (#3109)
    * chore(LiveCodeExample): extract `baseScope` to dedicated file
    
    * chore(component-generator): update to use sections

--- src/GeneratedTestComponent/docs/index.story.js
+++ src/GeneratedTestComponent/docs/index.story.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import LiveCodeExample from '../../../stories/utils/LiveCodeExample';
-import { Category } from '../../../stories/storiesHierarchy';
+import {
+  header,
+  tabs,
+  tab,
+  description,
+  importExample,
+  title,
+  columns,
+  divider,
+  code as baseCode,
+  playground,
+  api,
+  testkit
+} from 'wix-storybook-utils/Sections';
+
+import { storySettings } from '../test/storySettings';
+import allComponents from '../../../stories/utils/allComponents';
+
 import GeneratedTestComponent from '..';
 
+const code = config => baseCode({ components: allComponents, ...config });
+
 export default {
-  category: Category.TESTS,
+  category: storySettings.category,
   storyName: 'GeneratedTestComponent',
 
   component: GeneratedTestComponent,
   componentPath: '..',
 
   componentProps: {
-    buttonText: undefined,
+    buttonText: 'Hello World!',
   },
 
   exampleProps: {
@@ -19,18 +37,57 @@ export default {
     // https://github.com/wix/wix-ui/blob/master/packages/wix-storybook-utils/docs/usage.md#using-list
   },
 
-  examples: (
-    <div style={{ maxWidth: 627 }}>
-      <LiveCodeExample
-        compact
-        title="Live code example"
-        initialCode={`
-<GeneratedTestComponent
-  dataHook="story-generated-test-component-live-example"
-  buttonText="Press me for a surprise"
-  />
-        `}
-      />
-    </div>
-  ),
+  sections: [
+    header({
+      issueUrl: 'https://github.com/wix/wix-style-react/issues/new',
+      sourceUrl:
+        'https://github.com/wix/wix-style-react/tree/master/src/GeneratedTestComponent/',
+      component: <GeneratedTestComponent buttonText="Click me!"/>
+    }),
+
+    tabs([
+      tab({
+        title: 'Description',
+        sections: [
+          columns([
+            description({
+              title: 'Description',
+              text: 'This line here should briefly describe component in just a sentence or two. It should be short and easy to read.'
+            }),
+          ]),
+
+          columns([
+            importExample("import GeneratedTestComponent from 'wix-style-react/GeneratedTestComponent'"),
+          ]),
+
+          divider(),
+
+          title('Examples'),
+
+          columns([
+            description({
+              title: 'Simple Usage',
+              text: 'A simple example with compact preview'
+            }),
+            code({
+              compact: true,
+              source: '<GeneratedTestComponent buttonText="Hello World!"/>'
+            })
+          ]),
+
+          code({
+            title: 'Full Interactive Preview',
+            description: 'A non compact version of same code example as above',
+            source: '<GeneratedTestComponent buttonText="Hello World!"/>'
+          })
+        ]
+      }),
+
+      ...[
+        { title: 'API', sections: [api()] },
+        { title: 'Testkit', sections: [testkit()] },
+        { title: 'Playground', sections: [playground()] },
+      ].map(tab)
+    ])
+  ],
 };
