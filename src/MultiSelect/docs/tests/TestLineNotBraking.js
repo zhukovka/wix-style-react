d6a8536399660d77f87554d84b8c9b64d440eb37
moshek <moshek@wix.com>
2019-03-19 16:54:16 +0200

    remove "upgrade"

--- src/MultiSelect/docs/tests/TestLineNotBraking.js
+++ src/MultiSelect/docs/tests/TestLineNotBraking.js
@@ -1,18 +1,19 @@
-import MultiSelect from "../../MultiSelect"
-import React from "react";
+import MultiSelect from '../../MultiSelect';
+import React from 'react';
 
-export default () => (<div style={{ width: '400px' }}>
-  <MultiSelect
-    dataHook="multi-select-line-not-braking"
-    tags={[
-      { id: '1', label: 'a' },
-      { id: '2', label: 'b' },
-      { id: '3', label: 'c' },
-      { id: '4', label: 'd' },
-      { id: '5', label: 'e' },
-      { id: '6', label: 'f' },
-      { id: '7', label: 'f' },
-    ]}
-    upgrade
-  />
-</div>)
+export default () => (
+  <div style={{ width: '400px' }}>
+    <MultiSelect
+      dataHook="multi-select-line-not-braking"
+      tags={[
+        { id: '1', label: 'a' },
+        { id: '2', label: 'b' },
+        { id: '3', label: 'c' },
+        { id: '4', label: 'd' },
+        { id: '5', label: 'e' },
+        { id: '6', label: 'f' },
+        { id: '7', label: 'f' },
+      ]}
+    />
+  </div>
+);
