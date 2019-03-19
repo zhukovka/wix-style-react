b2773ba58e4f22b1562726855cca41f1b5af74e8
Erez Makavy <erezm@wix.com>
2019-03-19 13:57:00 +0200

    Ignore __mocks__ in testkit exports generator

--- scripts/generate-testkit-exports/list-all-components.js
+++ scripts/generate-testkit-exports/list-all-components.js
@@ -16,6 +16,7 @@ const NON_COMPONENT_FOLDER_NAMES = [
   'Deprecated',
   'Typography',
   'Animations',
+  '__mocks__',
 ];
 
 const matches = haystack => needle => haystack.some(h => needle === h);
