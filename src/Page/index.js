a4951d0e7fe0b21ed693e3f8b424c10226e40977
moshek <moshek@wix.com>
2019-04-03 16:02:34 +0300

    add deprecation log to page without "upgrade"

--- src/Page/index.js
+++ src/Page/index.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import PageDeprecated from './Page.deprecated';
 import PageNew from './Page';
 import { allValidators } from '../utils/propTypes';
+import deprecationLog from '../utils/deprecationLog';
 
 const Page = props => {
   const { upgrade, ...rest } = props;
@@ -12,20 +13,14 @@ const Page = props => {
 };
 
 Page.propTypes = {
-  upgrade: allValidators(
-    PropTypes.bool,
-    (/*props, propName, componentName*/) => {
-      /* depLog disabled until tests arr written and QA goes over it */
-      // if (!props[propName]) {
-      //       deprecationLog(`
-      // ${componentName}: New Layout API ! Please set upgrade=true prop to use new Layout API.
-      // When enabled, the page will use height: 100% and not require a parent of 'display: flex;flex-flow: column;'.
-      // Also Page.Content's may grow using 'min-height: inherit'. Supports Page.Sticky. New header minimization approach.
-      // See docs for more info: https://github.com/wix/wix-style-react/blob/master/src/Page/README.MIGRATION.md
-      // `);
-      // }
-    },
-  ),
+  upgrade: allValidators(PropTypes.bool, (props, propName, componentName) => {
+    if (!props[propName]) {
+      deprecationLog(`
+      ${componentName}: New Page API ! Please set upgrade=true prop to use new Page API.
+      See migration docs for more info: https://github.com/wix/wix-style-react/blob/master/src/Page/README.MIGRATION.md
+      `);
+    }
+  }),
 };
 
 Page.displayName = 'Page';
