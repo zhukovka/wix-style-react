c4b4e52da10eba2c66f2316c7b35efd96f3802b2
Yaniv Efraim <yaniv.efraim@gmail.com>
2019-04-17 16:34:21 +0300

    change sharding

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,15 +1,20 @@
+const multiCapabilities = new Array(10).fill({
+  browserName: 'chrome',
+});
+
 module.exports.config = {
   specs: ['test/**/*.e2e.js', 'src/**/*.e2e.js'],
   baseUrl: `http://localhost:6006/`,
   jasmineNodeOpts: { defaultTimeoutInterval: 120000 },
-  capabilities: {
-    browserName: 'chrome',
-    shardTestFiles: true,
-    maxInstances: 8,
-    // chromeOptions: {
-    //   args: ['--headless', '--window-size=1050,1075'],
-    // },
-  },
+  multiCapabilities,
+  // capabilities: {
+  //   browserName: 'chrome',
+  //   shardTestFiles: true,
+  //   maxInstances: 8,
+  //   // chromeOptions: {
+  //   //   args: ['--headless', '--window-size=1050,1075'],
+  //   // },
+  // },
   onPrepare() {
     require('./test/protractor-register');
     browser.ignoreSynchronization = true;
