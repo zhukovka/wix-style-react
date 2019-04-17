d112b610fbe01f9c694e7c8b752dffe87a03b4fc
Yaniv Efraim <yaniv.efraim@gmail.com>
2019-04-17 15:44:25 +0300

    wip - test parallel protractor

--- protractor.conf.js
+++ protractor.conf.js
@@ -2,7 +2,16 @@ module.exports.config = {
   specs: ['test/**/*.e2e.js', 'src/**/*.e2e.js'],
   baseUrl: `http://localhost:6006/`,
   jasmineNodeOpts: { defaultTimeoutInterval: 120000 },
+  capabilities: {
+    browserName: 'chrome',
+    shardTestFiles: true,
+    maxInstances: 8,
+    // chromeOptions: {
+    //   args: ['--headless', '--window-size=1050,1075'],
+    // },
+  },
   onPrepare() {
+    require('./test/protractor-register');
     browser.ignoreSynchronization = true;
   },
 };
---
+++ test/protractor-register.js
@@ -0,0 +1,16 @@
+require('regenerator-runtime/runtime');
+const sass = require('node-sass');
+const { wixCssModulesRequireHook } = require('yoshi-runtime');
+
+require('yoshi/src/require-hooks');
+
+const rootDir = './src';
+
+wixCssModulesRequireHook(rootDir, {
+  preprocessCss: (data, file) =>
+    sass.renderSync({
+      data,
+      file,
+      includePaths: ['node_modules', 'node_modules/compass-mixins/lib'],
+    }).css,
+});
