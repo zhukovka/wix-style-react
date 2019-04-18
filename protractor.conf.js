4de13d65492f1a25dbb6af92b2d1dc23e46b8ef4
Yaniv Efraim <yaniv.efraim@gmail.com>
2019-04-18 12:35:24 +0300

    revert

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,20 +1,15 @@
-const multiCapabilities = new Array(3).fill({
-  browserName: 'chrome',
-});
-
 module.exports.config = {
   specs: ['test/**/*.e2e.js', 'src/**/*.e2e.js'],
   baseUrl: `http://localhost:6006/`,
   jasmineNodeOpts: { defaultTimeoutInterval: 120000 },
-  multiCapabilities,
-  // capabilities: {
-  //   browserName: 'chrome',
-  //   shardTestFiles: true,
-  //   maxInstances: 8,
-  //   // chromeOptions: {
-  //   //   args: ['--headless', '--window-size=1050,1075'],
-  //   // },
-  // },
+  capabilities: {
+    browserName: 'chrome',
+    shardTestFiles: true,
+    maxInstances: 8,
+    // chromeOptions: {
+    //   args: ['--headless', '--window-size=1050,1075'],
+    // },
+  },
   onPrepare() {
     require('./test/protractor-register');
     browser.ignoreSynchronization = true;
