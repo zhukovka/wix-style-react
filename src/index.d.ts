76784e9b1c1313dd21f1ccbd5a085a385ede7ad7
Gilad Segal <gilads@wix.com>
2019-04-29 10:12:48 +0300

    Add icon types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,7 @@
 
+import {ReactElement} from 'react';
+
 export {default as Avatar, AvatarProps} from '../src/Avatar';
-//   export type Icon = React.ReactNode;
+export type IconElement = ReactElement<any>;
 //   type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 //   type Subtract<T, K extends T> = Pick<T, Exclude<keyof T, keyof K>>;
