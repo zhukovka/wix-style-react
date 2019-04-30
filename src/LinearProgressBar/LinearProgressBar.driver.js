a5cc4a42316b418a21533795598a5b2cef66233b
TalZa <talza@wix.com>
2019-04-30 14:04:45 +0300

    migrating LinearProgressBarDriverFactory driver from wix-ui-backoffice to WSR

--- src/LinearProgressBar/LinearProgressBar.driver.js
+++ src/LinearProgressBar/LinearProgressBar.driver.js
@@ -1,4 +1,30 @@
-// AutoDocs does not support `export from` syntax
-import { linearProgressBarDriverFactory } from 'wix-ui-backoffice/dist/src/components/LinearProgressBar/LinearProgressBar.driver';
+import { linearProgressBarDriverFactory as coreLinearProgressBarDriverFactory } from 'wix-ui-core/drivers/vanilla';
+import { tooltipDriverFactory } from 'wix-ui-backoffice/dist/src/components/Tooltip/Tooltip.driver';
+
+const linearProgressBarDriverFactory = ({ element, eventTrigger, wrapper }) => {
+  const createTooltipDriver = () =>
+    tooltipDriverFactory({
+      element: element.querySelector(
+        `[data-hook='linear-progressbar-tooltip']`,
+      ),
+      wrapper,
+      eventTrigger,
+    });
+  const coreProgressBarDriver = coreLinearProgressBarDriverFactory({
+    element,
+    wrapper,
+    eventTrigger,
+  });
+  const errorIcon = () => element.querySelector(`[data-hook='error-icon']`);
+  const successIcon = () => element.querySelector(`[data-hook='success-icon']`);
+
+  return {
+    ...coreProgressBarDriver,
+    isTooltipShown: () => createTooltipDriver().isContentElementExists(),
+    getTooltip: () => createTooltipDriver(),
+    isErrorIconShown: () => !!errorIcon(),
+    isSuccessIconShown: () => !!successIcon(),
+  };
+};
 
 export default linearProgressBarDriverFactory;
