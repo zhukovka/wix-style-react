5168417a59dd468926c4970da6b7b7d9063d6553
Gilad Segal <gilads@wix.com>
2019-04-30 15:20:33 +0300

    Revert "Fix CloseButton root export"
    This reverts commit 1aae5d66b70a2fdc060a80b88629a578a8ca7dfa.

--- src/index.d.ts
+++ src/index.d.ts
@@ -20,7 +20,7 @@ export {default as Card, CardProps} from './Card';
 export {default as CardGalleryItem, CardGalleryItemProps} from './CardGalleryItem';
 export {default as Checkbox, CheckboxProps} from './Checkbox';
 export {default as CircularProgressBar, CircularProgressBarProps} from './CircularProgressBar';
-export {default as CloseButton, CloseButtonProps} from './CloseButton';
+export {default as CloseButton, CloseButton} from './CloseButton';
 
 export {default as TextButton, TextButtonProps} from './TextButton';
 export {default as Thumbnail, ThumbnailProps} from './Thumbnail';
