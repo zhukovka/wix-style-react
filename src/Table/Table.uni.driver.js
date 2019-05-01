65777798e0e71c21380783411a2c03cd50817801
ritabazak <rita.bazak@gmail.com>
2019-05-01 14:48:46 +0300

    implemented all driver functions in the uniDriver. Still not working (comments are inside the uniDriver file)

--- src/Table/Table.uni.driver.js
+++ src/Table/Table.uni.driver.js
@@ -3,18 +3,41 @@ import { dataTableUniDriverFactory } from '../DataTable/DataTable.uni.driver';
 import { checkboxUniDriverFactory } from '../Checkbox/Checkbox.uni.driver';
 
 export const tableUniDriverFactory = base => {
-  const reactBase = ReactBase(base);
   const dataTableDriver = dataTableUniDriverFactory(base);
   const getRowCheckboxDriver = index =>
     checkboxUniDriverFactory(
-      //need to understand hot wrap native element with uniDriver
+      //need to understand how wrap native element with uniDriver
+      //(getCell returns native element)
       dataTableDriver.getCell(index, 0).$('[data-hook="row-select"]'),
     );
   const getBulkSelectionCheckboxDriver = () =>
     checkboxUniDriverFactory(
-      //need to understand hot wrap native element with uniDriver
+      //need to understand how wrap native element with uniDriver
+      //(getHeaderCell returns native element)
       dataTableDriver.getHeaderCell(0),
     ).$('[data-hook="table-select"]');
+
+  const isBulkSelectionChecked = async () => {
+    const checkboxDriver = getBulkSelectionCheckboxDriver();
+    return (
+      (await checkboxDriver.isChecked()) &&
+      !(await checkboxDriver.isIndeterminate())
+    );
+  };
+  const isBulkSelectionIndeterminate = async () => {
+    const checkboxDriver = getBulkSelectionCheckboxDriver();
+    return (
+      !(await checkboxDriver.isChecked()) &&
+      (await checkboxDriver.isIndeterminate())
+    );
+  };
+  const isBulkSelectionUnchecked = async () => {
+    const checkboxDriver = getBulkSelectionCheckboxDriver();
+    return (
+      !(await checkboxDriver.isChecked()) &&
+      !(await checkboxDriver.isIndeterminate())
+    );
+  };
 
   return {
     ...baseUniDriverFactory(base),
@@ -22,5 +45,31 @@ export const tableUniDriverFactory = base => {
     getRowCheckboxDriver,
     /** Get driver of row bulk-selection checbox */
     getBulkSelectionCheckboxDriver,
+    /** Whether bulk selection checkbox is disabled */
+    isBulkSelectionDisabled: getBulkSelectionCheckboxDriver().isDisabled(),
+    /** Whether specific row selection checkbox is disabled */
+    isRowSelectionDisabled: index => getRowCheckboxDriver(index).isDisabled(),
+    /** Click the row selection checkbox */
+    clickRowChecbox: index => getRowCheckboxDriver(index).click(),
+    /** Click the bulk-selection checkbox */
+    clickBulkSelectionCheckbox: () => getBulkSelectionCheckboxDriver().click(),
+    /** Is row selected by index */
+    isRowSelected: index => getRowCheckboxDriver(index).isChecked(),
+    getBulkSelectionState: async () => {
+      if (await isBulkSelectionChecked()) {
+        return 'ALL';
+      }
+      if (await isBulkSelectionIndeterminate()) {
+        return 'SOME';
+      }
+      if (await isBulkSelectionUnchecked()) {
+        return 'NONE';
+      }
+    },
+    /** Get title-bar (column titles) */
+    getTitlebar: async () => {
+      const titleBar = base.$('[data-hook="table-title-bar"]');
+      return (await titleBar.exists()) ? titleBar : null;
+    },
   };
 };
