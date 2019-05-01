a71a79f8f8c816f51eb681b545ba6ca74cbe49ae
Lior Chen <liorc@wix.com>
2019-05-01 15:57:26 +0300

    test(RadioButton): remove some of ReactBase usage

--- src/RadioGroup/RadioButton/RadioButton.uni.driver.js
+++ src/RadioGroup/RadioButton/RadioButton.uni.driver.js
@@ -1,18 +1,16 @@
 import { baseUniDriverFactory, ReactBase } from '../../../test/utils/unidriver';
 
 export const radioButtonUniDriverFactory = base => {
-  const reactBase = ReactBase(base);
-
-  const getRadioButton = async () => (await reactBase.children())[0];
-  const getLabel = async () => (await reactBase.children())[1];
+  const getRadioButton = () => base.$('input[type="radio"]');
+  const getLabel = () => base.$('[data-hook="radio-label"]');
   const getContent = () => base.$('[data-hook="radio-button-content"]');
 
   return {
     ...baseUniDriverFactory(base),
-    check: async () => ReactBase(await getRadioButton()).change(),
-    isChecked: async () => ReactBase(await getRadioButton()).prop('checked'),
-    isDisabled: async () => ReactBase(await getRadioButton()).prop('disabled'),
-    getLabel: async () => (await getLabel()).text(),
+    check: async () => getRadioButton().click(),
+    isChecked: async () => ReactBase(getRadioButton()).prop('checked'),
+    isDisabled: async () => ReactBase(getRadioButton()).prop('disabled'),
+    getLabel: async () => getLabel().text(),
     getContent: async () =>
       (await getContent().exists()) ? getContent().getNative() : null, // eslint-disable-line no-restricted-properties
   };
