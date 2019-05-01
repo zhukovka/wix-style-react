e2f8438c8749e61c66385cb154d0213f1c4e17fc
TalZa <talza@wix.com>
2019-05-01 18:43:02 +0300

    Adding interactive visual test (tooltip on icon hover)

--- src/LinearProgressBar/test/LinearProgressBarStories.js
+++ src/LinearProgressBar/test/LinearProgressBarStories.js
@@ -2,18 +2,39 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { getTestStoryKind } from '../../../stories/storiesHierarchy';
 import { storySettings } from './storySettings';
-
+import { linearProgressBarTestkitFactory } from '../../../testkit';
 import LinearProgressBar from '..';
 
 const TestStories = storiesOf(getTestStoryKind(storySettings), module);
 const { testStoryNames, dataHook } = storySettings;
 
 TestStories.add(testStoryNames.tooltipOnErrorProgressIndication, () => (
-  <LinearProgressBar
-    dataHook={dataHook}
-    errorMessage="some error message"
-    error
-    showProgressIndication
-    value={20}
-  />
+  <InteractiveEyeTest />
 ));
+
+const createLinearProgressBarDriver = dataHook =>
+  linearProgressBarTestkitFactory({
+    wrapper: document.body,
+    dataHook,
+  });
+
+class InteractiveEyeTest extends React.PureComponent {
+  componentDidMount() {
+    const driver = createLinearProgressBarDriver(dataHook);
+    driver.getTooltip().mouseEnter();
+  }
+
+  render() {
+    return (
+      <div style={{ padding: '40px' }}>
+        <LinearProgressBar
+          dataHook={dataHook}
+          errorMessage="some error message"
+          error
+          showProgressIndication
+          value={20}
+        />
+      </div>
+    );
+  }
+}
