ba8cac4a4e83bcfdddc3d67f8a7214d6df5e40ec
ritabazak <rita.bazak@gmail.com>
2019-05-01 14:06:54 +0300

    started to implement uniDriver functions.

--- src/Table/Table.uni.driver.js
+++ src/Table/Table.uni.driver.js
@@ -1,9 +1,26 @@
 import { baseUniDriverFactory, ReactBase } from '../../test/utils/unidriver';
+import { dataTableUniDriverFactory } from '../DataTable/DataTable.uni.driver';
+import { checkboxUniDriverFactory } from '../Checkbox/Checkbox.uni.driver';
 
 export const tableUniDriverFactory = base => {
   const reactBase = ReactBase(base);
+  const dataTableDriver = dataTableUniDriverFactory(base);
+  const getRowCheckboxDriver = index =>
+    checkboxUniDriverFactory(
+      //need to understand hot wrap native element with uniDriver
+      dataTableDriver.getCell(index, 0).$('[data-hook="row-select"]'),
+    );
+  const getBulkSelectionCheckboxDriver = () =>
+    checkboxUniDriverFactory(
+      //need to understand hot wrap native element with uniDriver
+      dataTableDriver.getHeaderCell(0),
+    ).$('[data-hook="table-select"]');
 
   return {
     ...baseUniDriverFactory(base),
+    /** Get driver of row selection checbox by row index */
+    getRowCheckboxDriver,
+    /** Get driver of row bulk-selection checbox */
+    getBulkSelectionCheckboxDriver,
   };
 };
